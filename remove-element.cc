324ab30497e3a1ab54dd4bcf4545f2d3c29d109e
lovebaonvwu <lovebaonvwu@gmail.com>
2019-04-22 11:14:54 +0800

    27. Remove Element

--- remove-element.cc
+++ remove-element.cc
@@ -1,14 +1,18 @@
-class Solution {
+class Solution
+{
 public:
-    int removeElement(vector<int>& nums, int val) {
+    int removeElement(vector<int> &nums, int val)
+    {
         int index = 0;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] != val) {
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (nums[i] != val)
+            {
                 nums[index++] = nums[i];
             }
         }
-        
+
         return index;
     }
 };
