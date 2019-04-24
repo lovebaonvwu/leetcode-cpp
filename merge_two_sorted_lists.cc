10e5f8b63958491cb651e79e3004c53e76f080b6
lovebaonvwu <lovebaonvwu@gmail.com>
2019-04-24 10:07:06 +0800

    21. Merge Two Sorted Lists

---
+++ merge_two_sorted_lists.cc
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if (!l1)
+      return l2;
+    if (!l2)
+      return l1;
+
+    if (l1->val < l2->val) {
+      l1->next = mergeTwoLists(l1->next, l2);
+      return l1;
+    } else {
+      l2->next = mergeTwoLists(l1, l2->next);
+      return l2;
+    }
+  }
+};  // 16ms
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode* head = new ListNode(0);
+    ListNode* cur = head;
+
+    while (l1 && l2) {
+      if (l1->val < l2->val) {
+        cur->next = l1;
+        l1 = l1->next;
+      } else {
+        cur->next = l2;
+        l2 = l2->next;
+      }
+
+      cur = cur->next;
+    }
+
+    if (l1)
+      cur->next = l1;
+    if (l2)
+      cur->next = l2;
+
+    return head->next;
+  }
+};  // 8ms
