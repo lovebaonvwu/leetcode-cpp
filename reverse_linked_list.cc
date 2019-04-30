0afb194ecf896147345972519ab80466850b0b1b
lovebaonvwu <lovebaonvwu@gmail.com>
2019-04-30 11:35:54 +0800

    206. Reverse Linked List

--- reverse_linked_list.cc
+++ reverse_linked_list.cc
@@ -45,3 +45,57 @@ class Solution {
     return reverse(next, head);
   }
 };  // 8ms
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* reverseList(ListNode* head) {
+    ListNode* prev = NULL;
+    ListNode* cur = head;
+    ListNode* next;
+
+    while (cur) {
+      next = cur->next;
+      cur->next = prev;
+
+      prev = cur;
+      cur = next;
+    }
+
+    return prev;
+  }
+};  // 12ms
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* reverseList(ListNode* head) {
+    ListNode* prev = NULL;
+    ListNode* cur = head;
+
+    while (cur) {
+      ListNode* next;
+      next = cur->next;
+      cur->next = prev;
+
+      prev = cur;
+      cur = next;
+    }
+
+    return prev;
+  }
+};  // 8ms
