2dbf8f99c24c55cf71d0daf348af24adb6949e86
lovebaonvwu <lovebaonvwu@gmail.com>
2019-05-10 19:50:20 +0800

    110. Balanced Binary Tree

--- balanced_binary_tree.cc
+++ balanced_binary_tree.cc
@@ -10,6 +10,39 @@
 class Solution {
  public:
   bool isBalanced(TreeNode* root) {
+    int height;
+
+    return balanced(root, height);
+  }
+
+  bool balanced(TreeNode* root, int& height) {
+    if (!root)
+      return true;
+
+    int lr = 0, rh = 0;
+
+    if (balanced(root->left, lr) && balanced(root->right, rh)) {
+      height = (lr > rh ? lr : rh) + 1;
+
+      return (lr - rh <= 1) && (lr - rh >= -1);
+    }
+
+    return false;
+  }
+};  // 12ms
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  bool isBalanced(TreeNode* root) {
     if (!root)
       return true;
 
@@ -30,4 +63,4 @@ class Solution {
 
     return 1 + (lh > rh ? lh : rh);
   }
-};
+};  // 16ms
