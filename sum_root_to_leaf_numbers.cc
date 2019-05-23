ad6c144467cc96eefc986a9f71b839105642f2fd
lovebaonvwu <lovebaonvwu@gmail.com>
2019-05-23 10:31:28 +0800

    129. Sum Root to Leaf Numbers

---
+++ sum_root_to_leaf_numbers.cc
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  int sumNumbers(TreeNode* root) {
+    int sum = 0;
+    int temp = 0;
+
+    sumNumber(root, temp, sum);
+
+    return sum;
+  }
+
+  void sumNumber(TreeNode* root, int temp, int& sum) {
+    if (!root) {
+      return;
+    }
+
+    temp = temp * 10 + root->val;
+    if (!root->left && !root->right) {
+      sum += temp;
+    }
+
+    sumNumber(root->left, temp, sum);
+    sumNumber(root->right, temp, sum);
+  }
+};
