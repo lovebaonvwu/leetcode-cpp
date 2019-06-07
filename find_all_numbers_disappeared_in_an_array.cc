0e162a602c96239c86c2e43876fbaa8f10da6b8a
lovebaonvwu <lovebaonvwu@gmail.com>
2019-06-07 22:01:56 +0800

    448. Find All Numbers Disappeared in an Array

---
+++ find_all_numbers_disappeared_in_an_array.cc
@@ -0,0 +1,21 @@
+class Solution {
+ public:
+  vector<int> findDisappearedNumbers(vector<int>& nums) {
+    vector<int> ans;
+
+    int n = nums.size();
+    for (int i = 0; i < n; ++i) {
+      while (nums[i] != nums[nums[i] - 1]) {
+        swap(nums[i], nums[nums[i] - 1]);
+      }
+    }
+
+    for (int i = 0; i < n; ++i) {
+      if (nums[i] != i + 1) {
+        ans.push_back(i + 1);
+      }
+    }
+
+    return ans;
+  }
+};  // 112ms
