996b5e4338c051aba54258c880e31810922cb8fd
lovebaonvwu <lovebaonvwu@gmail.com>
2019-06-07 21:35:47 +0800

    41. First Missing Positive

--- first_missing_positive.cc
+++ first_missing_positive.cc
@@ -1,6 +1,27 @@
 class Solution {
  public:
   int firstMissingPositive(vector<int>& nums) {
+    int n = nums.size();
+
+    for (int i = 0; i < n; ++i) {
+      while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+        swap(nums[nums[i] - 1], nums[i]);
+      }
+    }
+
+    for (int i = 0; i < n; ++i) {
+      if (nums[i] != i + 1) {
+        return i + 1;
+      }
+    }
+
+    return n + 1;
+  }
+};  // 4ms
+
+class Solution {
+ public:
+  int firstMissingPositive(vector<int>& nums) {
     unordered_map<int, int> mp;
     int max = INT_MIN;
 
