babb60d52e378c9854382b7c97a30410d8ee7a40
lovebaonvwu <lovebaonvwu@gmail.com>
2019-06-14 09:14:33 +0800

    557. Reverse Words in a String III

--- reverse_words_in_a_string_iii.cc
+++ reverse_words_in_a_string_iii.cc
@@ -24,6 +24,24 @@ class Solution {
 class Solution {
  public:
   string reverseWords(string s) {
+    int left = 0;
+
+    for (int i = 0; i < s.size(); ++i) {
+      if (s[i] == ' ') {
+        reverse(s.begin() + left, s.begin() + i);
+        left = i + 1;
+      }
+    }
+
+    reverse(s.begin() + left, s.end());
+
+    return s;
+  }
+};  // 20ms
+
+class Solution {
+ public:
+  string reverseWords(string s) {
     for (int i = 0; i < s.size();) {
       int j = i;
       for (; j < s.size() && s[j] != ' '; ++j) {
