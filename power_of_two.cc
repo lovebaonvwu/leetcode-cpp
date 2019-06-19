216193eb3897cd5aab68a4ab95ede0ed52e9491d
lovebaonvwu <lovebaonvwu@gmail.com>
2019-06-19 16:05:01 +0800

    231. Power of Two

---
+++ power_of_two.cc
@@ -0,0 +1,22 @@
+class Solution {
+ public:
+  bool isPowerOfTwo(int n) {
+    if (n <= 0) {
+      return false;
+    }
+
+    if (n == 1) {
+      return true;
+    }
+
+    while (n > 1) {
+      if (n % 2 != 0) {
+        return false;
+      }
+
+      n >>= 1;
+    }
+
+    return true;
+  }
+};
