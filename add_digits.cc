a879e673b07b8582921e4ba615310d5aa752ba72
lovebaonvwu <lovebaonvwu@gmail.com>
2019-06-20 08:51:11 +0800

    258. Add Digits

---
+++ add_digits.cc
@@ -0,0 +1,18 @@
+class Solution {
+ public:
+  int addDigits(int num) {
+    while (num > 9) {
+      int sum = 0;
+
+      while (num) {
+        sum += num % 10;
+
+        num /= 10;
+      }
+
+      num = sum;
+    }
+
+    return num;
+  }
+};  // 4ms
