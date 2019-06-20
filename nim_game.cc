22b0afeb98d041d3419c01d26e63c0c5cab62e9b
lovebaonvwu <lovebaonvwu@gmail.com>
2019-06-20 08:59:18 +0800

    292. Nim Game

--- nim_game.cc
+++ nim_game.cc
@@ -1,4 +1,9 @@
 class Solution {
  public:
   bool canWinNim(int n) { return n % 4 != 0; }
-};
+};  // 0ms
+
+class Solution {
+ public:
+  bool canWinNim(int n) { return (n & 3) != 0; }
+};  // 0ms
