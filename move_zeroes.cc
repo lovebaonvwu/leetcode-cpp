bbf852905506e23bce6bd15ff6f24d866746c675
lovebaonvwu <lovebaonvwu@gmail.com>
2019-06-25 23:58:00 +0800

    283. Move Zeroes

--- move_zeroes.cc
+++ move_zeroes.cc
@@ -3,6 +3,23 @@ class Solution {
   void moveZeroes(vector<int>& nums) {
     int slow = 0;
 
+    for (auto& n : nums) {
+      if (n != 0) {
+        nums[slow++] = n;
+      }
+    }
+
+    for (int i = slow; i < nums.size(); ++i) {
+      nums[i] = 0;
+    }
+  }
+};  // 12ms
+
+class Solution {
+ public:
+  void moveZeroes(vector<int>& nums) {
+    int slow = 0;
+
     for (int i = 0; i < nums.size(); ++i) {
       if (nums[i] == 0) {
         slow = i;
