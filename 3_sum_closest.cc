3634a0915e150fb3528c5659d271e3bb2445bcf5
lovebaonvwu <lovebaonvwu@gmail.com>
2019-07-15 08:50:31 +0800

    16. 3Sum Closest

--- 3_sum_closest.cc
+++ 3_sum_closest.cc
@@ -4,6 +4,51 @@ class Solution {
     int ans;
     int min = INT_MAX;
 
+    int n = nums.size();
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < n; ++i) {
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        continue;
+      }
+
+      int j = i + 1;
+      int k = n - 1;
+
+      while (j < k) {
+        int sum = nums[i] + nums[j] + nums[k];
+        int sub = abs(sum - target);
+
+        if (sub < min) {
+          min = sub;
+          ans = sum;
+        }
+
+        if (sum < target) {
+          do {
+            ++j;
+          } while (j < k && nums[j] == nums[j - 1]);
+        } else if (sum > target) {
+          do {
+            --k;
+          } while (j < k && nums[k] == nums[k + 1]);
+        } else {
+          return ans;
+        }
+      }
+    }
+
+    return ans;
+  }
+};  // 12ms
+
+class Solution {
+ public:
+  int threeSumClosest(vector<int>& nums, int target) {
+    int ans;
+    int min = INT_MAX;
+
     sort(nums.begin(), nums.end());
 
     int n = nums.size();
