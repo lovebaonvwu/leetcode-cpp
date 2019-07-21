c897e8ff71988b8441713e62539882874a9e649f
lovebaonvwu <lovebaonvwu@gmail.com>
2019-07-21 16:48:35 +0800

    198. House Robber

---
+++ house_robber.cc
@@ -0,0 +1,63 @@
+class Solution {
+ public:
+  int rob(vector<int>& nums) {
+    if (!nums.size()) {
+      return 0;
+    }
+
+    if (nums.size() == 1) {
+      return nums[0];
+    }
+
+    vector<int> dp(nums.size(), -1);
+
+    dp[nums.size() - 1] = nums[nums.size() - 1];
+    dp[nums.size() - 2] = nums[nums.size() - 2];
+
+    return dfs(nums, 0, dp);
+  }
+
+  int dfs(vector<int>& nums, int pos, vector<int>& dp) {
+    if (dp[pos] > -1) {
+      return max(dp[pos], pos + 1 < nums.size() ? dp[pos + 1] : 0);
+    }
+
+    dp[pos] = nums[pos] + dfs(nums, pos + 2, dp);
+
+    return max(dp[pos], dfs(nums, pos + 1, dp));
+  }
+};  // 0ms
+
+class Solution {
+ public:
+  int rob(vector<int>& nums) {
+    if (!nums.size()) {
+      return 0;
+    }
+
+    if (nums.size() == 1) {
+      return nums[0];
+    }
+
+    vector<int> dp(nums.size(), -1);
+
+    dp[nums.size() - 1] = nums[nums.size() - 1];
+    dp[nums.size() - 2] = nums[nums.size() - 2];
+
+    return dfs(nums, 0, dp);
+  }
+
+  int dfs(vector<int>& nums, int pos, vector<int>& dp) {
+    if (dp[pos] > -1) {
+      return max(dp[pos], pos + 1 < nums.size() ? dp[pos + 1] : 0);
+    }
+
+    dp[pos] = nums[pos] + dfs(nums, pos + 2, dp);
+
+    if (pos + 3 < nums.size()) {
+      dp[pos + 1] = nums[pos + 1] + dfs(nums, pos + 3, dp);
+    }
+
+    return max(dp[pos], pos + 1 < nums.size() ? dp[pos + 1] : 0);
+  }
+};  // 0ms
