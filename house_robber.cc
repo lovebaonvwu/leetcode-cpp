6b2bb2a7596998d4bd7830c0b59f7dd057b60a09
lovebaonvwu <lovebaonvwu@gmail.com>
2019-07-22 08:48:39 +0800

    198. House Robber

--- house_robber.cc
+++ house_robber.cc
@@ -61,3 +61,27 @@ class Solution {
     return max(dp[pos], pos + 1 < nums.size() ? dp[pos + 1] : 0);
   }
 };  // 0ms
+
+class Solution {
+ public:
+  int rob(vector<int>& nums) {
+    if (nums.size() == 0) {
+      return 0;
+    }
+
+    if (nums.size() == 1) {
+      return nums[0];
+    }
+
+    vector<int> dp(nums.size(), 0);
+
+    dp[nums.size() - 1] = nums[nums.size() - 1];
+    dp[nums.size() - 2] = max(nums[nums.size() - 1], nums[nums.size() - 2]);
+
+    for (int i = nums.size() - 3; i >= 0; --i) {
+      dp[i] = max(nums[i] + dp[i + 2], dp[i + 1]);
+    }
+
+    return max(dp[0], dp[1]);
+  }
+};  // 4ms
