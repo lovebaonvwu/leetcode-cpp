67d6b8ed27e7d77d92e26cf3cd0d2e3ba57d3547
lovebaonvwu <lovebaonvwu@gmail.com>
2019-07-24 10:00:55 +0800

    921. Minimum Add to Make Parentheses Valid

--- minimum_add_to_make_parentheses_valid.cc
+++ minimum_add_to_make_parentheses_valid.cc
@@ -14,3 +14,23 @@ class Solution {
     return stk.size();
   }
 };  // 0ms
+
+class Solution {
+ public:
+  int minAddToMakeValid(string S) {
+    int left = 0;
+    int right = 0;
+
+    for (auto& c : S) {
+      if (c == '(') {
+        ++left;
+      } else if (left > 0) {
+        --left;
+      } else {
+        ++right;
+      }
+    }
+
+    return left + right;
+  }
+};  // 4ms
