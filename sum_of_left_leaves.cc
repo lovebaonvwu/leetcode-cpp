8b99ddb7a5db3d1132fb9fd3ff4371df459cf8d4
lovebaonvwu <lovebaonvwu@gmail.com>
2019-08-14 17:28:19 +0800

    404. Sum of Left Leaves

--- sum_of_left_leaves.cc
+++ sum_of_left_leaves.cc
@@ -16,6 +16,32 @@ class Solution {
 
     int sum = 0;
 
+    if (root->left && !root->left->left && !root->left->right) {
+      sum += root->left->val;
+    }
+
+    return sum + sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+  }
+};  // 0ms
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  int sumOfLeftLeaves(TreeNode* root) {
+    if (!root) {
+      return 0;
+    }
+
+    int sum = 0;
+
     dfs(root->left, sum, true);
     dfs(root->right, sum, false);
 
