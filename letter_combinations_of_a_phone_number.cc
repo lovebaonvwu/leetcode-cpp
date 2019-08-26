a3d30c778094e130dc9c5ff566751a4cf2117e67
lovebaonvwu <lovebaonvwu@gmail.com>
2019-08-26 09:42:21 +0800

    17. Letter Combinations of a Phone Number

--- letter_combinations_of_a_phone_number.cc
+++ letter_combinations_of_a_phone_number.cc
@@ -9,6 +9,48 @@ class Solution {
                      "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
     vector<string> ans;
+
+    queue<string> q;
+    q.push("");
+
+    for (int i = 0; i < digits.size(); ++i) {
+      string arr = mp[digits[i] - '0'];
+
+      int cnt = q.size();
+      while (cnt > 0) {
+        for (int j = 0; j < arr.size(); ++j) {
+          string tmp = q.front();
+          tmp += arr[j];
+
+          q.push(tmp);
+        }
+
+        q.pop();
+
+        --cnt;
+      }
+    }
+
+    while (!q.empty()) {
+      ans.push_back(q.front());
+      q.pop();
+    }
+
+    return ans;
+  }
+};  // 4ms
+
+class Solution {
+ public:
+  vector<string> letterCombinations(string digits) {
+    if (digits.empty()) {
+      return {};
+    }
+
+    string mp[10] = {"",    "",    "abc",  "def", "ghi",
+                     "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    vector<string> ans;
     ans.push_back("");
 
     for (int i = 0; i < digits.size(); ++i) {
