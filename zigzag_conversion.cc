8b3cbee14f9dd9bebf817e24984bc5d554a81e1a
lovebaonvwu <lovebaonvwu@gmail.com>
2019-09-02 10:53:34 +0800

    6. ZigZag Conversion

--- zigzag_conversion.cc
+++ zigzag_conversion.cc
@@ -1,6 +1,38 @@
 class Solution {
  public:
   string convert(string s, int numRows) {
+    if (numRows <= 1) {
+      return s;
+    }
+
+    string ans = "";
+
+    for (int i = 0; i < s.size(); i += 2 * (numRows - 1)) {
+      ans += s[i];
+    }
+
+    for (int i = 1; i < numRows - 1; ++i) {
+      for (int j = i; j < s.size(); j += 2 * (numRows - 1)) {
+        ans += s[j];
+
+        int index = j + 2 * (numRows - i - 1);
+        if (index < s.size()) {
+          ans += s[index];
+        }
+      }
+    }
+
+    for (int i = numRows - 1; i < s.size(); i += 2 * (numRows - 1)) {
+      ans += s[i];
+    }
+
+    return ans;
+  }
+};  // 8ms
+
+class Solution {
+ public:
+  string convert(string s, int numRows) {
     if (numRows == 0) {
       return s;
     }
