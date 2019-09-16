7c0a62871211d1157e25d1da8f18c26920a2dd94
lovebaonvwu <lovebaonvwu@gmail.com>
2019-09-16 09:06:46 +0800

    890. Find and Replace Pattern

---
+++ find_and_replace_pattern.cc
@@ -0,0 +1,70 @@
+class Solution {
+ public:
+  vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+    vector<string> ans;
+
+    for (const string& w : words) {
+      if (w.size() != pattern.size()) {
+        continue;
+      }
+
+      bool match = true;
+      char w2p[26];
+      char p2w[26];
+
+      memset(w2p, 0, sizeof(w2p));
+      memset(p2w, 0, sizeof(p2w));
+
+      for (int i = 0; i < w.size(); ++i) {
+        if ((w2p[w[i] - 'a'] && w2p[w[i] - 'a'] != pattern[i]) ||
+            (p2w[pattern[i] - 'a'] && p2w[pattern[i] - 'a'] != w[i])) {
+          match = false;
+          break;
+        }
+
+        w2p[w[i] - 'a'] = pattern[i];
+        p2w[pattern[i] - 'a'] = w[i];
+      }
+
+      if (match) {
+        ans.push_back(w);
+      }
+    }
+
+    return ans;
+  }
+};  // 4ms
+
+class Solution {
+ public:
+  vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+    vector<string> ans;
+
+    for (const string& w : words) {
+      if (w.size() != pattern.size()) {
+        continue;
+      }
+
+      bool match = true;
+      unordered_map<char, char> w2p;
+      unordered_map<char, char> p2w;
+
+      for (int i = 0; i < w.size(); ++i) {
+        if ((w2p[w[i]] && w2p[w[i]] != pattern[i]) ||
+            (p2w[pattern[i]] && p2w[pattern[i]] != w[i])) {
+          match = false;
+          break;
+        }
+
+        w2p[w[i]] = pattern[i];
+        p2w[pattern[i]] = w[i];
+      }
+
+      if (match) {
+        ans.push_back(w);
+      }
+    }
+
+    return ans;
+  }
+};  // 8ms
