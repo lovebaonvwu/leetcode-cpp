76386d42f2624534020a236c148f31c62606f1dd
lovebaonvwu <lovebaonvwu@gmail.com>
2019-10-05 22:06:13 +0800

    78. Subsets

--- subsets.cc
+++ subsets.cc
@@ -22,3 +22,23 @@ class Solution {
 };
 // Runtime: 4 ms, faster than 97.68%
 // Memory Usage: 9.1 MB, less than 77.97%
+
+class Solution {
+ public:
+  vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> ans{{}};
+
+    for (const auto& n : nums) {
+      int sz = ans.size();
+
+      for (int i = 0; i < sz; ++i) {
+        ans.push_back(ans[i]);
+        ans.back().push_back(n);
+      }
+    }
+
+    return ans;
+  }
+};
+// Runtime: 8 ms, faster than 55.43%
+// Memory Usage: 9.1 MB, less than 93.22%
