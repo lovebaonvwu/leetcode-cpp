70f2103eb5a7f7c53b4cb478d9fcfd4233be5572
lovebaonvwu <lovebaonvwu@gmail.com>
2019-11-06 14:35:04 +0800

    1143. Longest Common Subsequence

--- longest_common_subsequence.cc
+++ longest_common_subsequence.cc
@@ -1,6 +1,35 @@
 class Solution {
  public:
   int longestCommonSubsequence(string text1, string text2) {
+    vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1));
+
+    for (int i = 0; i < dp[0].size(); ++i) {
+      dp[0][i] = 0;
+    }
+
+    for (int i = 0; i < dp.size(); ++i) {
+      dp[i][0] = 0;
+    }
+
+    for (int i = 1; i < dp.size(); ++i) {
+      for (int j = 1; j < dp[i].size(); ++j) {
+        if (text1[i - 1] == text2[j - 1]) {
+          dp[i][j] = 1 + dp[i - 1][j - 1];
+        } else {
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+
+    return dp[text1.size()][text2.size()];
+  }
+};
+// Runtime: 12 ms, faster than 88.66%
+// Memory Usage: 14.8 MB, less than 100.00%
+
+class Solution {
+ public:
+  int longestCommonSubsequence(string text1, string text2) {
     vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1, -1));
     return lcs(text1, text2, text1.size(), text2.size(), dp);
   }
