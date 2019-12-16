7093ff4eeb07f2e5d2f8cd04011edc4dab38ad7e
lovebaonvwu <lovebaonvwu@gmail.com>
2019-12-16 08:53:44 +0800

    1290. Convert Binary Number in a Linked List to Integer

---
+++ convert_binary_number_in_a_linked_list_to_integer.cc
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  int getDecimalValue(ListNode* head) {
+    int ans = 0;
+
+    while (head) {
+      ans = ans * 2 + head->val;
+      head = head->next;
+    }
+
+    return ans;
+  }
+};
+// Runtime: 4 ms, faster than 40.00%
+// Memory Usage: 8.4 MB, less than 100.00%
