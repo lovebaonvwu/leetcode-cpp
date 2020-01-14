55afd6d9037ab23f009bb63c410a37973d36c95d
lovebaonvwu <lovebaonvwu@gmail.com>
2020-01-14 11:22:34 +0800

    1315. Sum of Nodes with Even-Valued Grandparent

--- sum_of_nodes_with_even_valued_grandparent.cc
+++ sum_of_nodes_with_even_valued_grandparent.cc
@@ -12,6 +12,39 @@ class Solution {
 
  public:
   int sumEvenGrandparent(TreeNode* root) {
+    dfs(root, 1, 1);
+
+    return sum;
+  }
+
+  void dfs(TreeNode* root, int p, int gp) {
+    if (!root) {
+      return;
+    }
+
+    if (gp % 2 == 0) {
+      sum += root->val;
+    }
+
+    dfs(root->left, root->val, p);
+    dfs(root->right, root->val, p);
+  }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  int sum = 0;
+
+ public:
+  int sumEvenGrandparent(TreeNode* root) {
     dfs(root);
 
     return sum;
