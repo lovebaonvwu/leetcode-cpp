681110e4ebae04788357390e11df2d284bbc55cb
lovebaonvwu <lovebaonvwu@gmail.com>
2020-01-22 12:01:52 +0800

    1324. Print Words Vertically

---
+++ print_words_vertically.cc
@@ -0,0 +1,42 @@
+class Solution {
+ public:
+  vector<string> printVertically(string s) {
+    istringstream iss(s);
+
+    vector<string> words;
+
+    int len = 0;
+    while (iss) {
+      string val;
+      iss >> val;
+      len = max(len, (int)val.size());
+
+      if (val.size() > 0) {
+        words.push_back(val);
+      }
+    }
+
+    vector<string> ans;
+
+    for (int i = 0; i < len; ++i) {
+      string a;
+      for (auto& word : words) {
+        if (i < word.size()) {
+          a.append(1, word[i]);
+        } else {
+          a.append(1, ' ');
+        }
+      }
+      int j = a.size() - 1;
+
+      while (a[j] == ' ')
+        --j;
+
+      ans.push_back(a.substr(0, j + 1));
+    }
+
+    return ans;
+  }
+};
+// Runtime: 0 ms, faster than 100.00%
+// Memory Usage: 8.4 MB, less than 100.00%
