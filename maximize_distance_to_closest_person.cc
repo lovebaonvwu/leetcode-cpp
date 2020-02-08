14e9af947ee7d9e761adab47ce5f1a9c1fd09e51
lovebaonvwu <lovebaonvwu@gmail.com>
2020-02-08 12:09:07 +0800

    849. Maximize Distance to Closest Person

--- maximize_distance_to_closest_person.cc
+++ maximize_distance_to_closest_person.cc
@@ -2,6 +2,31 @@ class Solution {
  public:
   int maxDistToClosest(vector<int>& seats) {
     int n = seats.size();
+    int ans = 0;
+    int last = -1;
+
+    for (int i = 0; i < n; ++i) {
+      if (seats[i]) {
+        ans = last < 0 ? i : max(ans, (i - last) / 2);
+
+        last = i;
+      }
+    }
+
+    if (seats[n - 1] == 0) {
+      ans = max(ans, n - 1 - last);
+    }
+
+    return ans;
+  }
+};
+// Runtime: 12 ms, faster than 89.33%
+// Memory Usage: 10 MB, less than 58.82%
+
+class Solution {
+ public:
+  int maxDistToClosest(vector<int>& seats) {
+    int n = seats.size();
 
     vector<int> left(n);
     vector<int> right(n);
