c3da07a4486d0d5887061f31af758e40836232f3
lovebaonvwu <lovebaonvwu@gmail.com>
2020-05-31 18:41:48 +0800

    1460. Make Two Arrays Equal by Reversing Sub-arrays

--- make_two_arrays_equal_by_reversing_sub_arrays.cc
+++ make_two_arrays_equal_by_reversing_sub_arrays.cc
@@ -28,3 +28,19 @@ class Solution {
 };
 // Runtime: 16 ms, faster than 100.00%
 // Memory Usage: 14 MB, less than 100.00%
+
+class Solution {
+ public:
+  bool canBeEqual(vector<int>& target, vector<int>& arr) {
+    vector<int> cnt(1001);
+
+    for (int i = 0; i < target.size(); ++i) {
+      ++cnt[target[i]];
+      --cnt[arr[i]];
+    }
+
+    return (all_of(cnt.begin(), cnt.end(), [](int n) { return n == 0; }));
+  }
+};
+// Runtime: 16 ms, faster than 100.00%
+// Memory Usage: 14.6 MB, less than 100.00%
