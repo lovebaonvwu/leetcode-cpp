af9820a7dfd4496f635735707d3ca9139d3000ff
lovebaonvwu <lovebaonvwu@gmail.com>
2020-06-04 15:14:00 +0800

    344. Reverse String

--- reverse_string.cc
+++ reverse_string.cc
@@ -14,3 +14,16 @@ class Solution {
     }
   }
 };
+
+class Solution {
+ public:
+  void reverseString(vector<char>& s) {
+    size_t n = s.size();
+    for (int i = 0; i < n / 2; ++i) {
+      swap(s[i], s[n - i - 1]);
+    }
+  }
+};
+// Runtime: 48 ms
+// Memory Usage : 23.3 MB
+// Your runtime beats 82.38 % of cpp submissions
