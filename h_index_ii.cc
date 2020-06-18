fdcf57293166e1b6e5c1550149e84bfa6041b8cb
lovebaonvwu <lovebaonvwu@gmail.com>
2020-06-18 16:46:36 +0800

    275. H-Index II

--- h_index_ii.cc
+++ h_index_ii.cc
@@ -23,3 +23,33 @@ class Solution {
 };
 // Runtime: 44 ms, faster than 47.92%
 // Memory Usage: 18.8 MB, less than 37.26%
+
+class Solution {
+ public:
+  int hIndex(vector<int>& citations) {
+    int n = citations.size();
+
+    vector<int> cnt(n + 1);
+
+    for (int c : citations) {
+      if (c > n) {
+        ++cnt[n];
+      } else {
+        ++cnt[c];
+      }
+    }
+
+    int sum = 0;
+    for (int i = n; i >= 0; --i) {
+      sum += cnt[i];
+
+      if (sum >= i) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+};
+// Runtime: 44 ms, faster than 47.92%
+// Memory Usage: 19.6 MB, less than 5.36%
