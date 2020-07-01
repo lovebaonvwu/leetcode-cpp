27d81a8c528ebb8d7c2e96e5264527026afac597
lovebaonvwu <lovebaonvwu@gmail.com>
2020-07-01 15:39:26 +0800

    441. Arranging Coins

--- arranging_coins.cc
+++ arranging_coins.cc
@@ -26,3 +26,27 @@ class Solution {
     return i - 2;
   }
 };  // 12ms
+
+class Solution {
+ public:
+  int arrangeCoins(int n) {
+    int lo = 1;
+    int hi = n;
+
+    int ans = 0;
+    while (lo <= hi) {
+      long long md = lo + (hi - lo) / 2;
+
+      if (md * (md + 1) / 2 <= n) {
+        ans = md;
+        lo = md + 1;
+      } else {
+        hi = md - 1;
+      }
+    }
+
+    return ans;
+  }
+};
+// Runtime: 4 ms, faster than 79.21%
+// Memory Usage: 6.1 MB, less than 21.61%
