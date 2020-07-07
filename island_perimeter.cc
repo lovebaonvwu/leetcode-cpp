cfd5cf21c64869feb5683c49dcfcc38b33d82a08
lovebaonvwu <lovebaonvwu@gmail.com>
2020-07-07 15:16:44 +0800

    463. Island Perimeter

--- island_perimeter.cc
+++ island_perimeter.cc
@@ -52,3 +52,28 @@ class Solution {
 };
 // Runtime: 60 ms, faster than 93.91%
 // Memory Usage: 15.4 MB, less than 100.00%
+
+class Solution {
+ public:
+  int islandPerimeter(vector<vector<int>>& grid) {
+    int cnt = 0;
+    int dup = 0;
+
+    for (int i = 0; i < grid.size(); ++i) {
+      for (int j = 0; j < grid[i].size(); ++j) {
+        if (grid[i][j]) {
+          ++cnt;
+
+          if (i > 0 && grid[i - 1][j])
+            ++dup;
+          if (j > 0 && grid[i][j - 1])
+            ++dup;
+        }
+      }
+    }
+
+    return 4 * cnt - 2 * dup;
+  }
+};
+// Runtime: 352 ms, faster than 7.41%
+// Memory Usage: 96 MB, less than 91.86%
