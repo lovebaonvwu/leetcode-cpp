c1c8db0354eb4acfb5b405c6f1a7c5c73f6d1c50
lovebaonvwu <lovebaonvwu@gmail.com>
2020-07-08 15:49:13 +0800

    15. 3Sum

--- 3_sum.cc
+++ 3_sum.cc
@@ -44,3 +44,53 @@ class Solution {
     return ans;
   }
 };  // 104ms
+
+class Solution {
+ public:
+  vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> ans;
+
+    unordered_map<int, int> mp;
+
+    for (auto n : nums) {
+      ++mp[n];
+    }
+
+    sort(nums.begin(), nums.end());
+
+    int n = nums.size();
+
+    int* previ = nullptr;
+    for (int i = 0; i < n; ++i) {
+      if (previ && *previ == nums[i]) {
+        continue;
+      }
+
+      previ = &nums[i];
+
+      --mp[nums[i]];
+
+      int* prevj = nullptr;
+      for (int j = i + 1; j < n; ++j) {
+        if (prevj && *prevj == nums[j]) {
+          continue;
+        }
+
+        prevj = &nums[j];
+
+        --mp[nums[j]];
+
+        if (mp[-(nums[i] + nums[j])] > 0 && -(nums[i] + nums[j]) >= nums[j]) {
+          ans.push_back({nums[i], nums[j], -(nums[i] + nums[j])});
+        }
+
+        ++mp[nums[j]];
+      }
+      ++mp[nums[i]];
+    }
+
+    return ans;
+  }
+};
+// Runtime: 2488 ms, faster than 6.23%
+// Memory Usage: 46.6 MB, less than 15.75%
