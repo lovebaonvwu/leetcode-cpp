aeacf3e05aacf2182c3bea8c7b5b87887cace0a2
lovebaonvwu <lovebaonvwu@gmail.com>
2020-10-19 22:27:07 +0800

    123. Best Time to Buy and Sell Stock III

--- best_time_to_buy_and_sell_stock_iii.cc
+++ best_time_to_buy_and_sell_stock_iii.cc
@@ -18,3 +18,23 @@ class Solution {
 };
 // Runtime: 16 ms, faster than 59.39%
 // Memory Usage: 13.2 MB, less than 67.38%
+
+class Solution {
+ public:
+  int maxProfit(vector<int>& prices) {
+    int n = prices.size();
+    vector<vector<int>> dp(3, vector<int>(n));
+
+    for (int i = 1; i < 3; ++i) {
+      int tmpMax = dp[i - 1][0] - prices[0];
+      for (int j = 1; j < n; ++j) {
+        dp[i][j] = max(dp[i][j - 1], prices[j] + tmpMax);
+        tmpMax = max(tmpMax, dp[i - 1][j] - prices[j]);
+      }
+    }
+
+    return dp[2][n - 1];
+  }
+};
+// Runtime: 252 ms, faster than 88.26%
+// Memory Usage: 87.9 MB, less than 5.07%
