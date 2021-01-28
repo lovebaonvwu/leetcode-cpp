e57a871e30de35f1bade43368486eb64935862e2
lovebaonvwu <lovebaonvwu@gmail.com>
2021-01-28 10:06:10 +0800

    1680. Concatenation of Consecutive Binary Numbers

--- concatenation_of_consecutive_binary_numbers.cc
+++ concatenation_of_consecutive_binary_numbers.cc
@@ -93,3 +93,26 @@ class Solution {
 };
 // Runtime: 76 ms, faster than 70.36%
 // Memory Usage: 5.9 MB, less than 99.39%
+
+class Solution {
+ public:
+  int concatenatedBinary(int n) {
+    long ans = 0;
+    const int mod = 1000000007;
+
+    for (int i = 1; i <= n; ++i) {
+      int x = i, len = 0;
+
+      while (x > 0) {
+        ++len;
+        x /= 2;
+      }
+
+      ans = ((ans << len) + i) % mod;
+    }
+
+    return ans % mod;
+  }
+};
+// Runtime: 56 ms, faster than 77.43%
+// Memory Usage: 5.9 MB, less than 98.32%
