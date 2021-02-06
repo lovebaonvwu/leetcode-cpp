7d88c99ba1d83903170dd6d27adf1f46fd0937d2
lovebaonvwu <lovebaonvwu@gmail.com>
2021-02-06 16:10:45 +0800

    199. Binary Tree Right Side View

--- binary_tree_right_side_view.cc
+++ binary_tree_right_side_view.cc
@@ -47,3 +47,102 @@ class Solution {
     return ans;
   }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  vector<int> rightSideView(TreeNode* root) {
+    vector<int> ans;
+
+    if (!root) {
+      return ans;
+    }
+
+    queue<TreeNode*> q;
+    q.push(root);
+    int cnt = 1;
+    int rightVal = 0;
+
+    while (!q.empty()) {
+      cnt = q.size();
+
+      while (cnt--) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        rightVal = node->val;
+
+        if (node->left) {
+          q.push(node->left);
+        }
+
+        if (node->right) {
+          q.push(node->right);
+        }
+      }
+
+      ans.push_back(rightVal);
+    }
+
+    return ans;
+  }
+};
+// Runtime: 0 ms, faster than 100.00%
+// Memory Usage: 11.8 MB, less than 72.35%
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+ public:
+  vector<int> rightSideView(TreeNode* root) {
+    vector<int> ans;
+
+    if (!root) {
+      return ans;
+    }
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+      int cnt = q.size();
+      int rightVal = 0;
+
+      while (cnt--) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        rightVal = node->val;
+
+        if (node->left) {
+          q.push(node->left);
+        }
+
+        if (node->right) {
+          q.push(node->right);
+        }
+      }
+
+      ans.push_back(rightVal);
+    }
+
+    return ans;
+  }
+};
+// Runtime: 4 ms, faster than 73.84%
+// Memory Usage: 11.8 MB, less than 72.35%
