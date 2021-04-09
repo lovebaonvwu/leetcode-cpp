aa21c9dcc0dfa54904c55484f17c88d45eeda2eb
lovebaonvwu <lovebaonvwu@gmail.com>
2021-04-09 16:15:14 +0800

    953. Verifying an Alien Dictionary

--- verifying_an_alien_dictionary.cc
+++ verifying_an_alien_dictionary.cc
@@ -73,3 +73,44 @@ class Solution {
     return a.size() < b.size() ? -1 : 1;
   }
 };  // 8ms
+
+class Solution {
+ private:
+  map<char, int> mp;
+
+ public:
+  bool isAlienSorted(vector<string>& words, string order) {
+    if (words.size() < 2) {
+      return true;
+    }
+
+    for (int i = 0; i < order.size(); ++i) {
+      mp[order[i]] = i;
+    }
+
+    for (int i = 1; i < words.size(); ++i) {
+      if (stringCompare(words[i - 1], words[i]) > 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  int stringCompare(string& a, string& b) {
+    int size = min(a.size(), b.size());
+
+    for (int i = 0; i < size; ++i) {
+      if (mp[a[i]] > mp[b[i]]) {
+        return 1;
+      } else if (mp[a[i]] < mp[b[i]]) {
+        return -1;
+      }
+    }
+
+    return a.size() <= b.size() ? -1 : 1;
+  }
+};
+// update for test cast:
+// [ "hello", "hello" ]
+// "abcdefghijklmnopqrstuvwxyz"
