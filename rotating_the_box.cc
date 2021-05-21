3fe4525c3802c92c092aa08b79320b5a9e040ec7
lovebaonvwu <lovebaonvwu@gmail.com>
2021-05-21 17:34:46 +0800

    1861. Rotating the Box

---
+++ rotating_the_box.cc
@@ -0,0 +1,33 @@
+class Solution {
+ public:
+  vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+    int row = box.size(), col = box[0].size();
+
+    for (auto& b : box) {
+      for (int i = col - 1, j = i; j >= 0; --j) {
+        if (b[j] == '*') {
+          i = j - 1;
+        } else if (b[j] == '#') {
+          if (b[i] == '.') {
+            b[i--] = '#';
+            b[j] = '.';
+          } else {
+            i = j - 1;
+          }
+        }
+      }
+    }
+
+    vector<vector<char>> ans(col, vector<char>(row));
+
+    for (int i = 0; i < col; ++i) {
+      for (int j = 0; j < row; ++j) {
+        ans[i][j] = box[row - 1 - j][i];
+      }
+    }
+
+    return ans;
+  }
+};
+// Runtime: 280 ms, faster than 64.36%
+// Memory Usage: 52.6 MB, less than 57.07%
