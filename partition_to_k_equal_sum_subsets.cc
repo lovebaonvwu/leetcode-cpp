f826feee3084124f12d4efc2aee4276205269a74
lovebaonvwu <lovebaonvwu@gmail.com>
2021-06-16 10:09:12 +0800

    698. Partition to K Equal Sum Subsets

---
+++ partition_to_k_equal_sum_subsets.cc
@@ -0,0 +1,49 @@
+class Solution {
+  int total;
+  int target;
+  vector<bool> used;
+
+ public:
+  bool canPartitionKSubsets(vector<int>& nums, int k) {
+    total = accumulate(nums.begin(), nums.end(), 0);
+
+    if (total % k != 0) {
+      return false;
+    }
+
+    target = total / k;
+
+    sort(nums.rbegin(), nums.rend());
+
+    used.resize(nums.size(), false);
+
+    return dfs(nums, 0, k, 0);
+  }
+
+  bool dfs(vector<int>& nums, int cur, int k, int sum) {
+    if (k == 0) {
+      return true;
+    }
+
+    if (sum > target) {
+      return false;
+    } else if (sum == target) {
+      --k;
+      return dfs(nums, 0, k, 0);
+    }
+
+    for (int i = cur; i < nums.size(); ++i) {
+      if (!used[i]) {
+        used[i] = true;
+        if (dfs(nums, i + 1, k, sum + nums[i])) {
+          return true;
+        }
+        used[i] = false;
+      }
+    }
+
+    return false;
+  }
+};
+// Runtime: 0 ms, faster than 100.00%
+// Memory Usage: 9.1 MB, less than 50.96%
