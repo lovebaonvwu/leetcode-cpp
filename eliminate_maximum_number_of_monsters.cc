bcda97d1c89691bafff32f5eb8cc3d32cc2d1ee8
lovebaonvwu <lovebaonvwu@gmail.com>
2021-07-06 16:13:59 +0800

    1921. Eliminate Maximum Number of Monsters

---
+++ eliminate_maximum_number_of_monsters.cc
@@ -0,0 +1,22 @@
+class Solution {
+ public:
+  int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+    for (int i = 0; i < dist.size(); ++i) {
+      dist[i] = dist[i] / speed[i] + (dist[i] % speed[i] > 0);
+    }
+
+    sort(dist.begin(), dist.end());
+
+    int ans = 0;
+
+    for (int i = 0; i < dist.size() && i < dist[i]; ++i) {
+      if (i < dist[i]) {
+        ++ans;
+      }
+    }
+
+    return ans;
+  }
+};
+// Runtime: 128 ms, faster than 90.64%
+// Memory Usage: 80.9 MB, less than 96.06%
