c6c3c7a5efb987197e650535b4d4a08c965a847a
lovebaonvwu <lovebaonvwu@gmail.com>
2021-07-09 15:52:07 +0800

    300. Longest Increasing Subsequence

--- longest_increasing_subsequence.cc
+++ longest_increasing_subsequence.cc
@@ -63,3 +63,25 @@ class Solution {
 };
 // Runtime: 44 ms, faster than 31.41%
 // Memory Usage: 9 MB, less than 10.94%
+
+class Solution {
+ public:
+  int lengthOfLIS(vector<int>& nums) {
+    vector<int> result;
+
+    for (auto n : nums) {
+      auto it = lower_bound(result.begin(), result.end(), n);
+
+      if (it == result.end()) {
+        result.push_back(n);
+      } else {
+        result[it - result.begin()] = n;
+      }
+    }
+
+    return result.size();
+  }
+};
+// Runtime: 12 ms, faster than 76.08%
+// Memory Usage: 10.3 MB, less than 96.36%
+// 2021.7.9 at 奥盛大厦
