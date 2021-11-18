e2878fc6bb4e9766079021ba3d4c296f5dfe8122
lovebaonvwu <lovebaonvwu@gmail.com>
2021-11-18 09:02:49 +0800

    448. Find All Numbers Disappeared in an Array

--- find_all_numbers_disappeared_in_an_array.cc
+++ find_all_numbers_disappeared_in_an_array.cc
@@ -19,3 +19,29 @@ class Solution {
     return ans;
   }
 };  // 112ms
+
+class Solution {
+ public:
+  vector<int> findDisappearedNumbers(vector<int>& nums) {
+    int n = nums.size();
+
+    vector<int> cnt(n);
+
+    for (auto n : nums) {
+      ++cnt[n - 1];
+    }
+
+    vector<int> ans;
+
+    for (int i = 0; i < n; ++i) {
+      if (cnt[i] == 0) {
+        ans.push_back(i + 1);
+      }
+    }
+
+    return ans;
+  }
+};
+// Runtime: 40 ms, faster than 97.58%
+// Memory Usage: 34.9 MB, less than 26.30%
+// 2021.11.18 at 奥盛大厦
