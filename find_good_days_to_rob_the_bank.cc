07116718ccc92bc119e16730553962353908dcc8
lovebaonvwu <lovebaonvwu@gmail.com>
2021-12-16 10:45:45 +0800

    2100. Find Good Days to Rob the Bank

--- find_good_days_to_rob_the_bank.cc
+++ find_good_days_to_rob_the_bank.cc
@@ -35,3 +35,36 @@ class Solution {
 // Runtime: 221 ms, faster than 15.81%
 // Memory Usage: 87.4 MB, less than 74.63%
 // 2021.12.16 at 奥盛大厦
+
+class Solution {
+ public:
+  vector<int> goodDaysToRobBank(vector<int>& security, int time) {
+    int n = security.size();
+
+    vector<int> prefix(n);
+    for (int i = 1; i < n; ++i) {
+      if (security[i] <= security[i - 1]) {
+        prefix[i] = prefix[i - 1] + 1;
+      }
+    }
+
+    vector<int> suffix(n);
+    for (int i = n - 2; i >= 0; --i) {
+      if (security[i] <= security[i + 1]) {
+        suffix[i] = suffix[i + 1] + 1;
+      }
+    }
+
+    vector<int> ans;
+    for (int i = time; i < n - time; ++i) {
+      if (prefix[i] >= time && suffix[i] >= time) {
+        ans.push_back(i);
+      }
+    }
+
+    return ans;
+  }
+};
+// Runtime: 124 ms, faster than 82.32%
+// Memory Usage: 89.8 MB, less than 9.46%
+// 2021.12.16 at 奥盛大厦
