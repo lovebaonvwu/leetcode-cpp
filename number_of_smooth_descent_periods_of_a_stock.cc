d63bd6f59d7280d64916dfd687cf006d203a8f1a
lovebaonvwu <lovebaonvwu@gmail.com>
2021-12-21 08:56:20 +0800

    2110. Number of Smooth Descent Periods of a Stock

--- number_of_smooth_descent_periods_of_a_stock.cc
+++ number_of_smooth_descent_periods_of_a_stock.cc
@@ -21,3 +21,27 @@ class Solution {
 // Runtime: 136 ms, faster than 83.59%
 // Memory Usage: 101.4 MB, less than 5.57%
 // 2021.12.21 at 奥盛大厦
+
+class Solution {
+ public:
+  long long getDescentPeriods(vector<int>& prices) {
+    int n = prices.size();
+
+    long long ans = 1, cnt = 1;
+
+    for (int i = 1; i < n; ++i) {
+      if (prices[i - 1] - prices[i] == 1) {
+        ++cnt;
+      } else {
+        cnt = 1;
+      }
+
+      ans += cnt;
+    }
+
+    return ans;
+  }
+};
+// Runtime: 140 ms, faster than 80.33%
+// Memory Usage: 93.2 MB, less than 41.40%
+// 2021.12.21 at 奥盛大厦
