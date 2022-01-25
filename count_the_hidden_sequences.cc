9f94afcc0fe9d5d857b0bfc728f3a98425f7b917
lovebaonvwu <lovebaonvwu@gmail.com>
2022-01-25 09:28:49 +0800

    2145. Count the Hidden Sequences

--- count_the_hidden_sequences.cc
+++ count_the_hidden_sequences.cc
@@ -23,3 +23,22 @@ class Solution {
 // Runtime: 292 ms, faster than 49.94%
 // Memory Usage: 106.6 MB, less than 69.04%
 // 2022.1.25 at 奥盛大厦
+
+class Solution {
+ public:
+  int numberOfArrays(vector<int>& differences, int lower, int upper) {
+    long hi = 0, lo = 0, cur = 0;
+
+    for (auto n : differences) {
+      cur += n;
+
+      lo = min(lo, cur);
+      hi = max(hi, cur);
+    }
+
+    return max(0L, (upper - lower) - (hi - lo) + 1);
+  }
+};
+// Runtime: 267 ms, faster than 60.53%
+// Memory Usage: 106.7 MB, less than 27.52%
+// 2022.1.25 at 奥盛大厦
