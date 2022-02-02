7ac42817311127c166abe4718d53fb1234f7a809
lovebaonvwu <lovebaonvwu@gmail.com>
2022-02-02 21:27:43 +0800

    1944. Number of Visible People in a Queue

--- number_of_visible_people_in_a_queue.cc
+++ number_of_visible_people_in_a_queue.cc
@@ -24,3 +24,29 @@ class Solution {
 // Runtime: 144 ms, faster than 96.79%
 // Memory Usage: 84.3 MB, less than 32.93%
 // 2022.2.2 at 茗筑美嘉
+
+class Solution {
+ public:
+  vector<int> canSeePersonsCount(vector<int>& heights) {
+    int n = heights.size();
+
+    vector<int> ans(n), stk;
+
+    for (int i = n - 1; i >= 0; --i) {
+      int cnt = 0;
+      while (!stk.empty() && heights[i] > heights[stk.back()]) {
+        ++cnt;
+        stk.pop_back();
+      }
+
+      ans[i] = cnt + !stk.empty();
+
+      stk.push_back(i);
+    }
+
+    return ans;
+  }
+};
+// Runtime: 368 ms, faster than 12.15%
+// Memory Usage: 84.6 MB, less than 24.20%
+// 2022.2.2 at 茗筑美嘉
