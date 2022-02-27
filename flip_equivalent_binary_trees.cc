51d2e04057904cffbb10920fade39fb452ee643b
lovebaonvwu <lovebaonvwu@gmail.com>
2022-02-27 11:41:34 +0800

    951. Flip Equivalent Binary Trees

---
+++ flip_equivalent_binary_trees.cc
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+    if (!root1 && !root2) {
+      return true;
+    }
+
+    if (!root1 || !root2) {
+      return false;
+    }
+
+    if (root1->val != root2->val) {
+      return false;
+    }
+
+    return flipEquiv(root1->left, root2->left) &&
+               flipEquiv(root1->right, root2->right) ||
+           flipEquiv(root1->left, root2->right) &&
+               flipEquiv(root1->right, root2->left);
+  }
+};
+// Runtime: 10 ms, faster than 17.63%
+// Memory Usage: 12.3 MB, less than 68.07%
+// 2022.2.27 at 茗筑美嘉
