bbdc18d75625085485e49a1c8a06b0ecd3646cca
lovebaonvwu <lovebaonvwu@gmail.com>
2022-03-05 22:33:47 +0800

    2176. Count Equal and Divisible Pairs in an Array

---
+++ count_equal_and_divisible_pairs_in_an_array.cc
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  int countPairs(vector<int>& nums, int k) {
+    int ans = 0;
+
+    for (int i = 0; i < nums.size(); ++i) {
+      for (int j = i + 1; j < nums.size(); ++j) {
+        if (nums[i] == nums[j] && ((i * j) % k == 0)) {
+          ++ans;
+        }
+      }
+    }
+
+    return ans;
+  }
+};
+// 2022.3.5 at 茗筑美嘉
