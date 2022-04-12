67faadda960ce55b45625347752393e4a6360b3c
lovebaonvwu <lovebaonvwu@gmail.com>
2022-04-12 08:53:28 +0800

    2235. Add Two Integers

---
+++ add_two_integers.cc
@@ -0,0 +1,7 @@
+class Solution {
+ public:
+  int sum(int num1, int num2) { return num1 + num2; }
+};
+// Runtime: 0 ms, faster than 100.00%
+// Memory Usage: 5.9 MB, less than 100.00%
+// 2022.4.12 at 奥盛大厦
