ab82e2d9a07f486c334b049013cc5cbd5006978f
lovebaonvwu <lovebaonvwu@gmail.com>
2022-05-11 09:11:58 +0800

    1641. Count Sorted Vowel Strings

--- count_sorted_vowel_strings.cc
+++ count_sorted_vowel_strings.cc
@@ -40,3 +40,25 @@ class Solution {
 };
 // Runtime: 0 ms, faster than 100.00%
 // Memory Usage: 6.3 MB, less than 36.56%
+
+class Solution {
+ public:
+  int countVowelStrings(int n) {
+    vector<vector<int>> dp(n + 1, vector<int>(6));
+
+    for (int j = 1; j < 6; ++j) {
+      dp[1][j] = 1;
+    }
+
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j < 6; ++j) {
+        dp[i][j] += dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+
+    return dp[n][5];
+  }
+};
+// Runtime: 2 ms, faster than 49.32%
+// Memory Usage: 6.3 MB, less than 22.78%
+// 2022.5.11 at 奥盛大厦
