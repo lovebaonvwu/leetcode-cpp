7db022df3e86b0f0aa6ef17945b418b65b309519
lovebaonvwu <lovebaonvwu@gmail.com>
2022-05-17 09:02:52 +0800

    2270. Number of Ways to Split Array

--- number_of_ways_to_split_array.cc
+++ number_of_ways_to_split_array.cc
@@ -21,3 +21,24 @@ class Solution {
 // Runtime: 201 ms, faster than 81.21%
 // Memory Usage: 92.7 MB, less than 32.04%
 // 2022.5.17 at 奥盛大厦
+
+class Solution {
+ public:
+  int waysToSplitArray(vector<int>& nums) {
+    long long right = accumulate(begin(nums), end(nums), 0LL);
+
+    long long left = 0;
+    int ans = 0;
+    for (int i = 0; i < nums.size() - 1; ++i) {
+      left += nums[i];
+      right -= nums[i];
+
+      ans += left >= right;
+    }
+
+    return ans;
+  }
+};
+// Runtime: 285 ms, faster than 30.14%
+// Memory Usage: 85.6 MB, less than 67.50%
+// 2022.5.17 at 奥盛大厦
