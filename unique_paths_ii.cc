cfdfe79d40d367ddd8825894fc60702728d7ee3e
lovebaonvwu <lovebaonvwu@gmail.com>
2022-05-20 09:41:20 +0800

    63. Unique Paths II

--- unique_paths_ii.cc
+++ unique_paths_ii.cc
@@ -40,3 +40,29 @@ class Solution {
 };
 // Runtime: 4 ms, faster than 75.27%
 // Memory Usage: 9 MB, less than 100.00%
+
+class Solution {
+ public:
+  int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    int m = obstacleGrid.size();
+    int n = obstacleGrid[0].size();
+
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+    dp[0][1] =
+        1;  // start point dp[1][0] is also ok. We need to init dp[1][1] = 1;
+
+    for (int i = 1; i <= m; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        if (obstacleGrid[i - 1][j - 1] == 0) {
+          dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+      }
+    }
+
+    return dp[m][n];
+  }
+};
+// Runtime: 8 ms, faster than 22.56%
+// Memory Usage: 7.9 MB, less than 41.40%
+// 2022.5.20 at 奥盛大厦
