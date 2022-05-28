220975b76f177666529d47119d37220249f7d7fa
lovebaonvwu <lovebaonvwu@gmail.com>
2022-05-28 17:30:34 +0800

    2259. Remove Digit From Number to Maximize Result

--- remove_digit_from_number_to_maximize_result.cc
+++ remove_digit_from_number_to_maximize_result.cc
@@ -16,3 +16,20 @@ class Solution {
 // Runtime: 0 ms, faster than 100.00%
 // Memory Usage: 6.9 MB, less than 39.11%
 // 2022.5.28 at 奥盛大厦
+
+class Solution {
+ public:
+  string removeDigit(string number, char digit) {
+    for (int i = 0; i < number.size() - 1; ++i) {
+      if (number[i] == digit && number[i] < number[i + 1]) {
+        return number.substr(0, i) + number.substr(i + 1);
+      }
+    }
+
+    auto pos = number.rfind(digit);
+    return number.substr(0, pos) + number.substr(pos + 1);
+  }
+};
+// Runtime: 0 ms, faster than 100.00%
+// Memory Usage: 6 MB, less than 91.92%
+// 2022.5.28 at 奥盛大厦
