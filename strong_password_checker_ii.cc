190948bbbf66034daacf55527bcd2ab5dc1efd6b
lovebaonvwu <lovebaonvwu@gmail.com>
2022-06-12 13:47:20 +0800

    2299. Strong Password Checker II

---
+++ strong_password_checker_ii.cc
@@ -0,0 +1,36 @@
+class Solution {
+ public:
+  bool strongPasswordCheckerII(string password) {
+    if (password.size() < 8) {
+      return false;
+    }
+
+    bool lower = false;
+    bool upper = false;
+    bool digit = false;
+    bool specl = false;
+
+    unordered_set<char> spe(begin("!@#$%^&*()-+"), end("!@#$%^&*()-+"));
+
+    for (int i = 0; i < password.size(); ++i) {
+      if (islower(password[i])) {
+        lower = true;
+      } else if (isupper(password[i])) {
+        upper = true;
+      } else if (isdigit(password[i])) {
+        digit = true;
+      } else if (spe.count(password[i])) {
+        specl = true;
+      }
+
+      if (i > 0 && password[i] == password[i - 1]) {
+        return false;
+      }
+    }
+
+    return lower && upper && digit && specl;
+  }
+};
+// Runtime: 7 ms, faster than 20.00%
+// Memory Usage: 6.4 MB, less than 20.00%
+// 2022.6.12 at 名筑美嘉
