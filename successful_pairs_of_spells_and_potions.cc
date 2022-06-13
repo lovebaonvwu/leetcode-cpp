fbc0a611074e5fabd8c95eae0621d7b1c13bc8ef
lovebaonvwu <lovebaonvwu@gmail.com>
2022-06-13 16:29:33 +0800

    2300. Successful Pairs of Spells and Potions

--- successful_pairs_of_spells_and_potions.cc
+++ successful_pairs_of_spells_and_potions.cc
@@ -32,3 +32,25 @@ class Solution {
 // Runtime: 305 ms, faster than 50.00%
 // Memory Usage: 97.2 MB, less than 50.00%
 // 2022.6.13 at 奥盛大厦
+
+class Solution {
+ public:
+  vector<int> successfulPairs(vector<int>& spells,
+                              vector<int>& potions,
+                              long long success) {
+    sort(potions.begin(), potions.end());
+
+    vector<int> ans;
+    for (auto& s : spells) {
+      long need = (success + s - 1) / s;
+      auto it = lower_bound(begin(potions), end(potions), need);
+
+      ans.push_back(potions.end() - it);
+    }
+
+    return ans;
+  }
+};
+// Runtime: 330 ms, faster than 50.00%
+// Memory Usage: 99.7 MB, less than 50.00%
+// 2022.6.13 at 奥盛大厦
