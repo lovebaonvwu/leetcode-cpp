b1eb5d06a2386c53041d8f7ca28cabc5ac6619ce
lovebaonvwu <lovebaonvwu@gmail.com>
2022-07-11 10:10:12 +0800

    2336. Smallest Number in Infinite Set

--- smallest_number_in_infinite_set.cc
+++ smallest_number_in_infinite_set.cc
@@ -35,3 +35,38 @@ class SmallestInfiniteSet {
 // Runtime: 141 ms, faster than 100.00%
 // Memory Usage: 46.4 MB, less than 100.00%
 // 2022.7.11 at 奥盛大厦
+
+class SmallestInfiniteSet {
+  int smallest;
+  set<int> smaller;
+
+ public:
+  SmallestInfiniteSet() { smallest = 1; }
+
+  int popSmallest() {
+    if (smaller.empty()) {
+      return smallest++;
+    }
+
+    int res = *smaller.begin();
+    smaller.erase(res);
+
+    return res;
+  }
+
+  void addBack(int num) {
+    if (num < smallest) {
+      smaller.insert(num);
+    }
+  }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
+// Runtime: 83 ms, faster than 100.00%
+// Memory Usage: 35.5 MB, less than 100.00%
+// 2022.7.11 at 奥盛大厦
