e1af150e29dbced2d6c559b51b62df3399012944
lovebaonvwu <lovebaonvwu@gmail.com>
2022-07-12 09:50:25 +0800

    473. Matchsticks to Square

--- matchsticks_to_square.cc
+++ matchsticks_to_square.cc
@@ -45,3 +45,46 @@ class Solution {
 };
 // Runtime: 216 ms, faster than 20.77%
 // Memory Usage: 9.7 MB, less than 73.79%
+
+class Solution {
+  int total;
+  int used[15] = {0};
+
+ public:
+  bool makesquare(vector<int>& matchsticks) {
+    total = accumulate(matchsticks.begin(), matchsticks.end(), 0);
+
+    if (matchsticks.size() < 4 || total % 4 != 0) {
+      return false;
+    }
+
+    return dfs(matchsticks, total / 4, 0, 0, 4);
+  }
+
+  bool dfs(vector<int>& m, int tar, int sum, int i, int side) {
+    if (side == 1) {
+      return true;
+    }
+
+    if (sum == tar) {
+      return dfs(m, tar, 0, 0, side - 1);
+    }
+
+    for (int j = i; j < m.size(); ++j) {
+      if (used[j] || sum + m[j] > tar) {
+        continue;
+      }
+
+      used[j] = 1;
+      if (dfs(m, tar, sum + m[j], j + 1, side)) {
+        return true;
+      }
+      used[j] = 0;
+    }
+
+    return false;
+  }
+};
+// Runtime: 629 ms, faster than 35.24%
+// Memory Usage: 10.1 MB, less than 66.64%
+// 2022.7.12 at 奥盛大厦
