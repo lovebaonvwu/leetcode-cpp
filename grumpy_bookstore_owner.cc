17262413076a252d1c846627f3803416cd26c90a
lovebaonvwu <lovebaonvwu@gmail.com>
2022-07-14 09:21:27 +0800

    1052. Grumpy Bookstore Owner

--- grumpy_bookstore_owner.cc
+++ grumpy_bookstore_owner.cc
@@ -24,3 +24,28 @@ class Solution {
 // Runtime: 48 ms, faster than 63.19%
 // Memory Usage: 31.9 MB, less than 86.99%
 // 2022.7.14 at 奥盛大厦
+
+class Solution {
+ public:
+  int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+    int ans = 0, n = customers.size();
+
+    int curmax = 0, gmax = 0;
+    for (int r = 0, l = 0; r < n; ++r) {
+      ans += grumpy[r] == 0 ? customers[r] : 0;
+      curmax += grumpy[r] == 1 ? customers[r] : 0;
+
+      if (r - l + 1 > minutes) {
+        curmax -= grumpy[l] == 1 ? customers[l] : 0;
+        ++l;
+      }
+
+      gmax = max(gmax, curmax);
+    }
+
+    return ans + gmax;
+  }
+};
+// Runtime: 48 ms, faster than 63.19%
+// Memory Usage: 32 MB, less than 55.58%
+// 2022.7.14 at 奥盛大厦
