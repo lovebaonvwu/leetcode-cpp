d13eef1236f7c6ce72e87fb150a90644024181ea
lovebaonvwu <lovebaonvwu@gmail.com>
2022-07-26 09:24:54 +0800

    2348. Number of Zero-Filled Subarrays

--- number_of_zero_filled_subarrays.cc
+++ number_of_zero_filled_subarrays.cc
@@ -19,3 +19,20 @@ class Solution {
 // Runtime: 282 ms, faster than 100.00%
 // Memory Usage: 107.5 MB, less than 50.00%
 // 2022.7.26 at 奥盛大厦
+
+class Solution {
+ public:
+  long long zeroFilledSubarray(vector<int>& nums) {
+    long long ans = 0;
+
+    for (int i = 0, cnt = 0; i < nums.size(); ++i) {
+      cnt = nums[i] == 0 ? cnt + 1 : 0;
+      ans += cnt;
+    }
+
+    return ans;
+  }
+};
+// Runtime: 278 ms, faster than 100.00%
+// Memory Usage: 107.6 MB, less than 50.00%
+// 2022.7.26 at 奥盛大厦
