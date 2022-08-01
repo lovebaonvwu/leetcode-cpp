bc0e826167d7b9d3ef45b6bb8d4e76c73512591d
lovebaonvwu <lovebaonvwu@gmail.com>
2022-08-01 09:19:32 +0800

    2358. Maximum Number of Groups Entering a Competition

--- maximum_number_of_groups_entering_a_competition.cc
+++ maximum_number_of_groups_entering_a_competition.cc
@@ -16,3 +16,25 @@ class Solution {
 // Runtime: 102 ms, faster than 100.00%
 // Memory Usage: 56.6 MB, less than 100.00%
 // 2022.8.1 at 奥盛大厦
+
+class Solution {
+ public:
+  int maximumGroups(vector<int>& grades) {
+    int lo = 1, hi = 1000, n = grades.size();
+
+    while (lo < hi) {
+      int k = (lo + hi + 1) / 2;
+
+      if ((1 + k) * k / 2 > n) {
+        hi = k - 1;
+      } else {
+        lo = k;
+      }
+    }
+
+    return lo;
+  }
+};
+// Runtime: 128 ms, faster than 100.00%
+// Memory Usage: 56.5 MB, less than 100.00%
+// 2022.8.1 at 奥盛大厦
