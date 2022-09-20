891b99529312da79a73159db4a0ed42329f12dba
lovebaonvwu <lovebaonvwu@gmail.com>
2022-09-20 13:53:26 +0800

    718. Maximum Length of Repeated Subarray

--- maximum_length_of_repeated_subarray.cc
+++ maximum_length_of_repeated_subarray.cc
@@ -66,3 +66,34 @@ class Solution {
 // Runtime: 284 ms, faster than 42.49%
 // Memory Usage: 108.8 MB, less than 45.28%
 // 2021.7.8 at 奥盛大厦
+
+class Solution {
+ public:
+  int findLength(vector<int>& A, vector<int>& B) {
+    int m = A.size();
+    int n = B.size();
+
+    vector<vector<int>> dp(m, vector<int>(n));
+
+    int ans = 0;
+
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (A[i] == B[j]) {
+          if (i == 0 || j == 0) {
+            dp[i][j] = 1;
+          } else {
+            dp[i][j] = dp[i - 1][j - 1] + 1;
+          }
+        }
+
+        ans = max(ans, dp[i][j]);
+      }
+    }
+
+    return ans;
+  }
+};
+// Runtime: 451 ms, faster than 57.64%
+// Memory Usage: 109 MB, less than 54.50%
+// 2022.9.20 at 奥盛大厦
