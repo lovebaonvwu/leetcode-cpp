d5578e03e07a80851baf090941582961eafb82b6
lovebaonvwu <lovebaonvwu@gmail.com>
2022-09-24 11:49:21 +0800

    2413. Smallest Even Multiple

---
+++ smallest_even_multiple.cc
@@ -0,0 +1,7 @@
+class Solution {
+ public:
+  int smallestEvenMultiple(int n) { return n % 2 == 0 ? n : n * 2; }
+};
+// Runtime: 7 ms, faster than 8.59%
+// Memory Usage: 6 MB, less than 24.61%
+// 2022.9.24 at 茗筑美嘉
