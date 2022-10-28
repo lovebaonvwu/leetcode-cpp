87cfc09fe72231aa34ece3632998cc2ce4beb835
lovebaonvwu <lovebaonvwu@gmail.com>
2022-10-28 09:39:03 +0800

    1753. Maximum Score From Removing Stones

--- maximum_score_from_removing_stones.cc
+++ maximum_score_from_removing_stones.cc
@@ -22,3 +22,21 @@ class Solution {
 // Runtime: 170 ms, faster than 46.27%
 // Memory Usage: 5.9 MB, less than 93.88%
 // 2022.10.28 at 奥盛大厦
+
+class Solution {
+ public:
+  int maximumScore(int a, int b, int c) {
+    if (a < b) {
+      return maximumScore(b, a, c);
+    }
+
+    if (b < c) {
+      return maximumScore(a, c, b);
+    }
+
+    return b == 0 ? 0 : 1 + maximumScore(a - 1, b - 1, c);
+  }
+};
+// Runtime: 7 ms, faster than 71.08%
+// Memory Usage: 8.2 MB, less than 5.12%
+// 2022.10.28 at 奥盛大厦
