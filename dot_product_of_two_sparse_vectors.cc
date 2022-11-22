0b9be10db4ed4556866ec1595ff36138bfe7554c
lovebaonvwu <lovebaonvwu@gmail.com>
2022-11-22 23:08:35 +0800

    1570. Dot Product of Two Sparse Vectors

--- dot_product_of_two_sparse_vectors.cc
+++ dot_product_of_two_sparse_vectors.cc
@@ -26,3 +26,67 @@ class SparseVector {
 // Runtime 1388 ms
 // Memory 286.3 MB
 // 2022.11.22 at 茗筑美嘉
+
+class SparseVector {
+  unordered_map<int, int> mp;
+
+ public:
+  SparseVector(vector<int>& nums) {
+    for (int i = 0; i < nums.size(); ++i) {
+      if (nums[i] > 0) {
+        mp[i] = nums[i];
+      }
+    }
+  }
+
+  // Return the dotProduct of two sparse vectors
+  int dotProduct(SparseVector& vec) {
+    int ans = 0;
+    for (auto& [index, val] : mp) {
+      ans += vec.mp[index] * val;
+    }
+
+    return ans;
+  }
+};
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1(nums1);
+// SparseVector v2(nums2);
+// int ans = v1.dotProduct(v2);
+// Runtime 486 ms
+// Memory 170 MB
+// 2022.11.22 at 茗筑美嘉
+
+class SparseVector {
+  unordered_map<int, int> mp;
+
+ public:
+  SparseVector(vector<int>& nums) {
+    for (int i = 0; i < nums.size(); ++i) {
+      if (nums[i] > 0) {
+        mp[i] = nums[i];
+      }
+    }
+  }
+
+  // Return the dotProduct of two sparse vectors
+  int dotProduct(SparseVector& vec) {
+    int ans = 0;
+    for (auto& [index, val] : mp) {
+      if (vec.mp.find(index) != vec.mp.end()) {
+        ans += vec.mp[index] * val;
+      }
+    }
+
+    return ans;
+  }
+};
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1(nums1);
+// SparseVector v2(nums2);
+// int ans = v1.dotProduct(v2);
+// Runtime 241 ms
+// Memory 168.6 MB
+// 2022.11.22 at 茗筑美嘉
