eeb1ce7a66dede6f6d9a5ea175152b1f770e8538
lovebaonvwu <lovebaonvwu@gmail.com>
2023-01-13 10:25:42 +0900

    2246. Longest Path With Different Adjacent Characters

--- longest_path_with_different_adjacent_characters.cc
+++ longest_path_with_different_adjacent_characters.cc
@@ -43,3 +43,48 @@ class Solution {
 // Runtime 716 ms
 // Memory 252.2 MB
 // 2023.1.13 at 奥盛大厦
+
+class Solution {
+  unordered_map<int, vector<int>> tree;
+  int ans = 0;
+
+ public:
+  int longestPath(vector<int>& parent, string s) {
+    for (int i = 0; i < parent.size(); ++i) {
+      tree[parent[i]].push_back(i);
+    }
+
+    return max(ans, dfs(0, s));
+  }
+
+  int dfs(int root, const string& s) {
+    int firstMaxCnt = 0;
+    int secondMaxCnt = 0;
+
+    if (tree[root].empty()) {
+      return 1;
+    }
+
+    for (auto next : tree[root]) {
+      int nextCnt = dfs(next, s);
+
+      if (s[root] == s[next]) {
+        continue;
+      }
+
+      if (nextCnt > firstMaxCnt) {
+        secondMaxCnt = firstMaxCnt;
+        firstMaxCnt = nextCnt;
+      } else if (nextCnt > secondMaxCnt) {
+        secondMaxCnt = nextCnt;
+      }
+    }
+
+    ans = max(ans, firstMaxCnt + secondMaxCnt + 1);
+
+    return firstMaxCnt + 1;
+  }
+};
+// Runtime 701 ms
+// Memory 248.7 MB
+// 2023.1.13 at 奥盛大厦
