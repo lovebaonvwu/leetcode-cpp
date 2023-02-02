49c8c80692f8302071dd1a615f34b743fe0eb2f7
lovebaonvwu <lovebaonvwu@gmail.com>
2023-02-02 11:25:50 +0900

    163. Missing Ranges

--- missing_ranges.cc
+++ missing_ranges.cc
@@ -37,3 +37,32 @@ class Solution {
 // Runtime 0 ms
 // Memory 7.1 MB
 // 2023.2.2 at 聊城月季西路长江路
+
+class Solution {
+ public:
+  vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
+    int prev = lower - 1;
+    nums.push_back(upper + 1);
+
+    vector<string> ans;
+    for (int i = 0; i < nums.size(); ++i) {
+      if (nums[i] > prev + 1) {
+        int a = prev + 1;
+        int b = nums[i] - 1;
+
+        if (a == b) {
+          ans.push_back(to_string(a));
+        } else {
+          ans.push_back(to_string(a) + "->" + to_string(b));
+        }
+      }
+
+      prev = nums[i];
+    }
+
+    return ans;
+  }
+};
+// Runtime 0 ms
+// Memory 7 MB
+// 2023.2.2 at 聊城月季西路长江路
