e276b7bbc09466c1caa9db3ec9cf41557e1de1db
lovebaonvwu <lovebaonvwu@gmail.com>
2023-02-08 14:03:02 +0900

    280. Wiggle Sort

--- wiggle_sort.cc
+++ wiggle_sort.cc
@@ -13,3 +13,18 @@ class Solution {
 // Runtime 17 ms
 // Memory 13.6 MB
 // 2023.2.8 at 聊城市东昌路怡莱酒店
+
+class Solution {
+ public:
+  void wiggleSort(vector<int>& nums) {
+    for (int i = 0; i < nums.size() - 1; ++i) {
+      if ((i % 2 == 0 && nums[i] > nums[i + 1]) ||
+          ((i % 2 == 1 && nums[i] < nums[i + 1]))) {
+        swap(nums[i], nums[i + 1]);
+      }
+    }
+  }
+};
+// Runtime 15 ms
+// Memory 13.6 MB
+// 2023.2.8 at 聊城市东昌路怡莱酒店
