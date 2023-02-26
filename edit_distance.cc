d8f1e8c93d698fc6e66b4b3b9e45999240ce6602
lovebaonvwu <lovebaonvwu@gmail.com>
2023-02-26 19:50:41 +0900

    72. Edit Distance

--- edit_distance.cc
+++ edit_distance.cc
@@ -30,3 +30,45 @@ class Solution {
 // Runtime 19 ms
 // Memory 9.1 MB
 // 2023.2.26 at 茗筑美嘉
+
+class Solution {
+  vector<vector<int>> memo;
+
+ public:
+  int minDistance(string word1, string word2) {
+    int l1 = word1.size();
+    int l2 = word2.size();
+
+    memo = vector<vector<int>>(l1 + 1, vector<int>(l2 + 1, -1));
+
+    return dfs(word1, word2, l1, l2);
+  }
+
+  int dfs(string& w1, string& w2, int l1, int l2) {
+    if (l1 == 0) {
+      return l2;
+    }
+
+    if (l2 == 0) {
+      return l1;
+    }
+
+    if (memo[l1][l2] >= 0) {
+      return memo[l1][l2];
+    }
+
+    int ret;
+    if (w1[l1 - 1] == w2[l2 - 1]) {
+      ret = dfs(w1, w2, l1 - 1, l2 - 1);
+    } else {
+      ret = min(dfs(w1, w2, l1 - 1, l2 - 1),
+                min(dfs(w1, w2, l1 - 1, l2), dfs(w1, w2, l1, l2 - 1))) +
+            1;
+    }
+
+    return memo[l1][l2] = ret;
+  }
+};
+// Runtime 17 ms
+// Memory 9 MB
+// 2023.2.26 at 茗筑美嘉
