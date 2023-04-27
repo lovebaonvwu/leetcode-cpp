94ef48452ec2cf68c21e8d1a89864f6012c2af53
lovebaonvwu <lovebaonvwu@gmail.com>
2023-04-27 22:31:54 +0900

    2652. Sum Multiples

---
+++ sum_multiples.cc
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  int sumOfMultiples(int n) {
+    int ans = 0;
+
+    for (int i = 1; i <= n; ++i) {
+      if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+        ans += i;
+      }
+    }
+
+    return ans;
+  }
+};
+// Runtime 4 ms
+// Memory 5.9 MB
