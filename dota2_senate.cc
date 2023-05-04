cfca086a63050668b5de64b6da4f94418d858f6b
lovebaonvwu <lovebaonvwu@gmail.com>
2023-05-05 00:02:46 +0900

    649. Dota2 Senate

---
+++ dota2_senate.cc
@@ -0,0 +1,32 @@
+class Solution {
+ public:
+  string predictPartyVictory(string senate) {
+    queue<int> rq, dq;
+    int n = senate.size();
+
+    for (int i = 0; i < n; ++i) {
+      if (senate[i] == 'R') {
+        rq.push(i);
+      } else {
+        dq.push(i);
+      }
+    }
+
+    while (!rq.empty() && !dq.empty()) {
+      int r = rq.front();
+      rq.pop();
+      int d = dq.front();
+      dq.pop();
+
+      if (r < d) {
+        rq.push(r + n);
+      } else {
+        dq.push(d + n);
+      }
+    }
+
+    return rq.empty() ? "Dire" : "Radiant";
+  }
+};
+// Runtime 6 ms
+// Memory 8 MB
