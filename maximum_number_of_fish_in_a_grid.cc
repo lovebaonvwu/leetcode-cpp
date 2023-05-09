bf5dd274ba476642ff4a5fd92bddbb12f0d9438e
lovebaonvwu <lovebaonvwu@gmail.com>
2023-05-09 10:27:05 +0900

    2658. Maximum Number of Fish in a Grid

--- maximum_number_of_fish_in_a_grid.cc
+++ maximum_number_of_fish_in_a_grid.cc
@@ -50,3 +50,38 @@ class Solution {
 };
 // Runtime 134 ms
 // Memory 108.5 MB
+
+class Solution {
+  int m;
+  int n;
+
+ public:
+  int findMaxFish(vector<vector<int>>& grid) {
+    m = grid.size();
+    n = grid[0].size();
+
+    int ans = 0;
+
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        ans = max(ans, dfs(grid, i, j));
+      }
+    }
+
+    return ans;
+  }
+
+  int dfs(vector<vector<int>>& g, int y, int x) {
+    if (y == m || y < 0 || x == n || x < 0 || g[y][x] == 0) {
+      return 0;
+    }
+
+    int cnt = g[y][x];
+    g[y][x] = 0;
+
+    return cnt + dfs(g, y + 1, x) + dfs(g, y - 1, x) + dfs(g, y, x + 1) +
+           dfs(g, y, x - 1);
+  }
+};
+// Runtime 76 ms
+// Memory 88.5 MB
