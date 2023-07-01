1472d31eb939f40ba1d9bf5b97410f95ae6cf33a
lovebaonvwu <lovebaonvwu@gmail.com>
2023-07-01 18:14:34 +0900

    2744. Find Maximum Number of String Pairs

---
+++ find_maximum_number_of_string_pairs.cc
@@ -0,0 +1,18 @@
+class Solution {
+ public:
+  int maximumNumberOfStringPairs(vector<string>& words) {
+    int ans = 0;
+    int n = words.size();
+    for (int i = 0; i < n; ++i) {
+      for (int j = i + 1; j < n; ++j) {
+        if (words[i][0] == words[j][1] && words[i][1] == words[j][0]) {
+          ++ans;
+        }
+      }
+    }
+
+    return ans;
+  }
+};
+// Runtime 3 ms
+// Memory 20.5 MB
