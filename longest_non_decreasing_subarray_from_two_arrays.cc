310d9fa658f7ef6963eb69998793f9e06387903e
lovebaonvwu <lovebaonvwu@gmail.com>
2023-07-13 11:23:58 +0900

    2771. Longest Non-decreasing Subarray From Two Arrays

--- longest_non_decreasing_subarray_from_two_arrays.cc
+++ longest_non_decreasing_subarray_from_two_arrays.cc
@@ -35,3 +35,26 @@ class Solution {
 };
 // Runtime 286 ms
 // Memory 166.3 MB
+
+class Solution {
+ public:
+  int maxNonDecreasingLength(vector<int>& nums1, vector<int>& nums2) {
+    int dp1 = 1, dp2 = 1;
+    int n = nums1.size();
+    int ans = 1;
+
+    for (int j = 1; j < n; ++j) {
+      int t11 = nums1[j] >= nums1[j - 1] ? dp1 + 1 : 1;
+      int t12 = nums1[j] >= nums2[j - 1] ? dp2 + 1 : 1;
+      int t21 = nums2[j] >= nums1[j - 1] ? dp1 + 1 : 1;
+      int t22 = nums2[j] >= nums2[j - 1] ? dp2 + 1 : 1;
+      dp1 = max(t11, t12);
+      dp2 = max(t21, t22);
+      ans = max(ans, max(dp1, dp2));
+    }
+
+    return ans;
+  }
+};
+// Runtime 235 ms
+// Memory 160.1 MB
