ffaa34cdec5759db1cbb13cfa57688ca80b7fd5b
lovebaonvwu <lovebaonvwu@gmail.com>
2023-09-02 09:54:44 +0800

    2828. Check if a String Is an Acronym of Words

---
+++ check_if_a_string_is_an_acronym_of_words.cc
@@ -0,0 +1,18 @@
+class Solution {
+ public:
+  bool isAcronym(vector<string>& words, string s) {
+    if (words.size() != s.size()) {
+      return false;
+    }
+
+    for (int i = 0; i < s.size(); ++i) {
+      if (words[i][0] != s[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
+// 17ms
+// 38.73MB
