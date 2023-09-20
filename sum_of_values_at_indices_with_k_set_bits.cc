8dbd325ebd26bf5129887ecaa9d794412968cd3b
lovebaonvwu <lovebaonvwu@gmail.com>
2023-09-20 09:02:31 +0800

    2859. Sum of Values at Indices With K Set Bits

---
+++ sum_of_values_at_indices_with_k_set_bits.cc
@@ -0,0 +1,15 @@
+class Solution {
+ public:
+  int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+    int ans = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+      if (__builtin_popcount(i) == k) {
+        ans += nums[i];
+      }
+    }
+
+    return ans;
+  }
+};
+// 14ms
+// 21.89MB
