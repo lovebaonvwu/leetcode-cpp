97064f22a09ed22cc7ff4ebbe7fcba65cd5e9638
lovebaonvwu <lovebaonvwu@gmail.com>
2023-10-02 14:23:08 +0800

    2873. Maximum Value of an Ordered Triplet I

--- maximum_value_of_an_ordered_triplet_i.cc
+++ maximum_value_of_an_ordered_triplet_i.cc
@@ -16,3 +16,27 @@ class Solution {
 };
 // 14ms
 // 17.84MB
+
+class Solution {
+ public:
+  long long maximumTripletValue(vector<int>& nums) {
+    long long ans = 0;
+    int n = nums.size();
+    vector<int> rmax(n);
+    rmax[n - 1] = nums.back();
+    for (int i = n - 2; i >= 0; --i) {
+      rmax[i] = max(nums[i], rmax[i + 1]);
+    }
+    for (int i = 0; i < n; ++i) {
+      for (int j = i + 1; j < n - 1; ++j) {
+        if (nums[i] > nums[j]) {
+          ans = max(ans, 1LL * (nums[i] - nums[j]) * rmax[j + 1]);
+        }
+      }
+    }
+
+    return ans;
+  }
+};
+// 3ms
+// 18.30MB
