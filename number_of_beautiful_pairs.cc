e06307e10707376864d2eec42af677eb1b04bf38
lovebaonvwu <lovebaonvwu@gmail.com>
2023-10-06 12:24:06 +0800

    2748. Number of Beautiful Pairs

--- number_of_beautiful_pairs.cc
+++ number_of_beautiful_pairs.cc
@@ -23,3 +23,29 @@ class Solution {
 };
 // 48ms
 // 67.96MB
+
+class Solution {
+ public:
+  int countBeautifulPairs(vector<int>& nums) {
+    int n = nums.size();
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+      int a = 0;
+      int t = nums[i];
+      while (t > 0) {
+        a = t % 10;
+        t /= 10;
+      }
+      for (int j = i + 1; j < n; ++j) {
+        int b = nums[j] % 10;
+        if (__gcd(a, b) == 1) {
+          ++ans;
+        }
+      }
+    }
+
+    return ans;
+  }
+};
+// 36ms
+// 67.96MB
