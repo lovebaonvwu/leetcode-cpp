b14acdc7b48c59d37c069a1a14f041b350940d76
lovebaonvwu <lovebaonvwu@gmail.com>
2023-10-20 22:19:04 +0800

    2903. Find Indices With Index and Value Difference I

---
+++ find_indices_with_index_and_value_difference_i.cc
@@ -0,0 +1,19 @@
+class Solution {
+ public:
+  vector<int> findIndices(vector<int>& nums,
+                          int indexDifference,
+                          int valueDifference) {
+    int n = nums.size();
+    for (int i = 0; i < n; ++i) {
+      for (int j = i + indexDifference; j < n; ++j) {
+        if (abs(nums[i] - nums[j]) >= valueDifference) {
+          return {i, j};
+        }
+      }
+    }
+
+    return {-1, -1};
+  }
+};
+// 8ms
+// 17.86MB
