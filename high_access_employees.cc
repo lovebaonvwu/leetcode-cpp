4729a2a962e9e4e56efb7a184da72f4997b76750
lovebaonvwu <lovebaonvwu@gmail.com>
2023-11-15 09:52:25 +0800

    2933. High-Access Employees

--- high_access_employees.cc
+++ high_access_employees.cc
@@ -28,3 +28,37 @@ class Solution {
 };
 // 62ms
 // 44.35MB
+
+class Solution {
+ public:
+  vector<string> findHighAccessEmployees(vector<vector<string>>& access_times) {
+    unordered_map<string, vector<int>> mp;
+    for (auto& at : access_times) {
+      auto& name = at[0];
+      auto& time = at[1];
+
+      int hour = (time[0] - '0') * 10 + (time[1] - '0');
+      int min = (time[2] - '0') * 10 + (time[3] - '0');
+
+      mp[name].push_back(hour * 60 + min);
+    }
+
+    for (auto& [name, time] : mp) {
+      sort(time.begin(), time.end());
+    }
+
+    vector<string> ans;
+    for (auto& [name, time] : mp) {
+      for (int i = 2; i < time.size(); ++i) {
+        if (time[i] - time[i - 2] < 60) {
+          ans.push_back(name);
+          break;
+        }
+      }
+    }
+
+    return ans;
+  }
+};
+// 60ms
+// 44.56MB
