6b0ee19f17be4408c167a68f851305d4b2d6097b
lovebaonvwu <lovebaonvwu@gmail.com>
2023-11-26 18:26:02 +0800

    2942. Find Words Containing Character

---
+++ 2023/find_words_containing_character.cc
@@ -0,0 +1,18 @@
+class Solution {
+ public:
+  vector<int> findWordsContaining(vector<string>& words, char x) {
+    vector<int> ans;
+    for (int i = 0; i < words.size(); ++i) {
+      for (auto& c : words[i]) {
+        if (c == x) {
+          ans.push_back(i);
+          break;
+        }
+      }
+    }
+
+    return ans;
+  }
+};
+// 19ms
+// 28.89MB
