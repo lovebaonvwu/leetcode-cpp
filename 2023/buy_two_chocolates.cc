4b91300023ea7326862d2a9bd99dfdc9e1ce4dc1
lovebaonvwu <lovebaonvwu@gmail.com>
2023-12-09 20:32:59 +0800

    2706. Buy Two Chocolates

---
+++ 2023/buy_two_chocolates.cc
@@ -0,0 +1,11 @@
+class Solution {
+ public:
+  int buyChoco(vector<int>& prices, int money) {
+    sort(prices.begin(), prices.end());
+    if (prices[0] + prices[1] <= money)
+      return money - (prices[0] + prices[1]);
+    return money;
+  }
+};
+// 18ms
+// 46.98MB
