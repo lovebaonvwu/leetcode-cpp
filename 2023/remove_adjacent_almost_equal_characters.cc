b4f3e51296a6b7955a56cd43901a940436f6474c
lovebaonvwu <lovebaonvwu@gmail.com>
2023-12-11 09:51:56 +0800

    2957. Remove Adjacent Almost-Equal Characters

--- 2023/remove_adjacent_almost_equal_characters.cc
+++ 2023/remove_adjacent_almost_equal_characters.cc
@@ -14,3 +14,20 @@ class Solution {
 };
 // 0ms
 // 6.59MB
+
+class Solution {
+ public:
+  int removeAlmostEqualCharacters(string word) {
+    int ans = 0;
+    for (int i = 1; i < word.size(); ++i) {
+      if (abs(word[i] - word[i - 1]) <= 1) {
+        ++ans;
+        ++i;
+      }
+    }
+
+    return ans;
+  }
+};
+// 0ms
+// 6.61MB
