be477b6809cf94a69abb88a9de990ed42c1be211
lovebaonvwu <lovebaonvwu@gmail.com>
2023-12-16 20:11:39 +0800

    2960. Count Tested Devices After Test Operations

---
+++ 2023/count_tested_devices_after_test_operations.cc
@@ -0,0 +1,19 @@
+class Solution {
+ public:
+  int countTestedDevices(vector<int>& batteryPercentages) {
+    int ans = 0;
+    int n = batteryPercentages.size();
+    for (int i = 0; i < n; ++i) {
+      if (batteryPercentages[i] > 0) {
+        ++ans;
+        for (int j = i + 1; j < n; ++j) {
+          batteryPercentages[j] = max(batteryPercentages[j] - 1, 0);
+        }
+      }
+    }
+
+    return ans;
+  }
+};
+// 15ms
+// 17.71MB
