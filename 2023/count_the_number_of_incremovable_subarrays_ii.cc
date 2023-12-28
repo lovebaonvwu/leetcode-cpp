48e862303378dd0d51b2e88e61d69a0a6e24cce1
lovebaonvwu <lovebaonvwu@gmail.com>
2023-12-28 11:33:40 +0800

    2972. Count the Number of Incremovable Subarrays II

--- 2023/count_the_number_of_incremovable_subarrays_ii.cc
+++ 2023/count_the_number_of_incremovable_subarrays_ii.cc
@@ -23,3 +23,30 @@ class Solution {
 };
 // 311ms
 // 94.89MB
+
+class Solution {
+ public:
+  long long incremovableSubarrayCount(vector<int>& nums) {
+    int n = nums.size();
+    int j = n - 1;
+    while (j > 0 && nums[j] > nums[j - 1])
+      --j;
+
+    if (j == 0)
+      return 1LL * (1 + n) * n / 2;
+
+    long long ans = 0LL;
+    for (int i = 0, prev = 0; i < n; ++i) {
+      while (j < n && prev >= nums[j])
+        ++j;
+      ans += n - j + 1;
+      if (prev >= nums[i])
+        break;
+      prev = nums[i];
+    }
+
+    return ans;
+  }
+};
+// 224ms
+// 94.62MB
