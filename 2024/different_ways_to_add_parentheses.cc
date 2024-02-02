e50d5d1ea8b807307f1ced10c4f5a4fa516d2091
lovebaonvwu <lovebaonvwu@gmail.com>
2024-02-02 11:23:56 +0800

    241. Different Ways to Add Parentheses

--- 2024/different_ways_to_add_parentheses.cc
+++ 2024/different_ways_to_add_parentheses.cc
@@ -52,3 +52,64 @@ class Solution {
 };
 // 8 ms
 // 13.92 MB
+
+class Solution {
+  bool isNumber(const string& s) {
+    int i = 0;
+    while (i < s.size()) {
+      if (!isdigit(s[i]))
+        return false;
+      ++i;
+    }
+
+    return true;
+  }
+
+ public:
+  vector<int> diffWaysToCompute(string expression) {
+    unordered_map<string, vector<int>> cached;
+
+    function<vector<int>(const string&)> dfs =
+        [&](const string& s) -> vector<int> {
+      if (isNumber(s)) {
+        return {stoi(s)};
+      }
+
+      if (cached.find(s) != cached.end()) {
+        return cached[s];
+      }
+
+      vector<int> left;
+      vector<int> right;
+      vector<int> ret;
+
+      int n = s.size();
+      for (int i = 0; i < n; ++i) {
+        if (s[i] == '*' || s[i] == '+' || s[i] == '-') {
+          left = dfs(s.substr(0, i));
+          right = dfs(s.substr(i + 1));
+        }
+
+        for (auto l : left) {
+          for (auto r : right) {
+            if (s[i] == '*') {
+              ret.push_back(l * r);
+            } else if (s[i] == '+') {
+              ret.push_back(l + r);
+            } else if (s[i] == '-') {
+              ret.push_back(l - r);
+            }
+          }
+        }
+      }
+
+      return cached[s] = ret;
+    };
+
+    vector<int> ans = dfs(expression);
+
+    return ans;
+  }
+};
+// 0 ms
+// 9.18 MB
