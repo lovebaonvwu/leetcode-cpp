e0af419626e2caed91f1adb2eca3826f28b8e2e7
lovebaonvwu <lovebaonvwu@gmail.com>
2024-02-03 19:01:55 +0800

    3019. Number of Changing Keys

---
+++ 2024/number_of_changing_keys.cc
@@ -0,0 +1,15 @@
+class Solution {
+ public:
+  int countKeyChanges(string s) {
+    int ans = 0;
+    for (int i = 1; i < s.size(); ++i) {
+      if (tolower(s[i]) != tolower(s[i - 1])) {
+        ++ans;
+      }
+    }
+
+    return ans;
+  }
+};
+// 3 ms
+// 8.04 MB
