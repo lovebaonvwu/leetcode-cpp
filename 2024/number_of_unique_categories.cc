064aaedbc0e0aa87438b007cf2f835e8729f1aa2
lovebaonvwu <lovebaonvwu@gmail.com>
2024-02-16 13:24:24 +0800

    2782. Number of Unique Categories

--- 2024/number_of_unique_categories.cc
+++ 2024/number_of_unique_categories.cc
@@ -71,3 +71,80 @@ class Solution {
 };
 // 112 ms
 // 105.00 MB
+
+class DisjointSet {
+ public:
+  DisjointSet(int n) : size(n) {
+    parent.resize(size);
+    iota(begin(parent), end(parent), 0);
+
+    rank.resize(size);
+  }
+
+  int Find(int x) {
+    if (parent[x] != x) {
+      parent[x] = Find(parent[x]);
+    }
+
+    return parent[x];
+  }
+
+  void Union(int x, int y) {
+    int px = Find(x);
+    int py = Find(y);
+
+    if (px == py) {
+      return;
+    }
+
+    if (rank[px] > rank[py]) {
+      parent[py] = px;
+    } else if (rank[py] > rank[px]) {
+      parent[px] = py;
+    } else {
+      ++rank[px];
+      parent[py] = px;
+    }
+  }
+
+  int UniqueSet() {
+    int ret = 0;
+    for (int i = 0; i < size; ++i) {
+      ret += parent[i] == i;
+    }
+
+    return ret;
+  }
+
+ private:
+  vector<int> parent;
+  vector<int> rank;
+  int size;
+};
+
+/**
+ * Definition for a category handler.
+ * class CategoryHandler {
+ * public:
+ *     CategoryHandler(vector<int> categories);
+ *     bool haveSameCategory(int a, int b);
+ * };
+ */
+class Solution {
+ public:
+  int numberOfCategories(int n, CategoryHandler* categoryHandler) {
+    DisjointSet ds(n);
+
+    for (int i = 0; i < n; ++i) {
+      for (int j = i + 1; j < n; ++j) {
+        if (categoryHandler->haveSameCategory(i, j)) {
+          ds.Union(i, j);
+        }
+      }
+    }
+
+    return ds.UniqueSet();
+  }
+};
+// 109 ms
+// 105.08 MB
