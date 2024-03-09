290a85be1a51bf4a75822cc8bf10557d3be37f09
lovebaonvwu <lovebaonvwu@gmail.com>
2024-03-09 19:19:35 +0800

    3069. Distribute Elements Into Two Arrays I

---
+++ 2024/distribute_elements_into_two_arrays_i.cc
@@ -0,0 +1,24 @@
+class Solution {
+ public:
+  vector<int> resultArray(vector<int>& nums) {
+    vector<int> a1;
+    vector<int> a2;
+
+    a1.push_back(nums[0]);
+    a2.push_back(nums[1]);
+
+    for (int i = 2; i < nums.size(); ++i) {
+      if (a1.back() > a2.back()) {
+        a1.push_back(nums[i]);
+      } else {
+        a2.push_back(nums[i]);
+      }
+    }
+
+    a1.insert(a1.end(), a2.begin(), a2.end());
+
+    return a1;
+  }
+};
+// 3 ms
+// 22.97 MB
