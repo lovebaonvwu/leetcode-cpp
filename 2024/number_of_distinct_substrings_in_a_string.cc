78b456cec41d6bb13a47d37a768a906aae4546e8
lovebaonvwu <lovebaonvwu@gmail.com>
2024-07-11 16:02:00 +0800

    1698. Number of Distinct Substrings in a String

--- 2024/number_of_distinct_substrings_in_a_string.cc
+++ 2024/number_of_distinct_substrings_in_a_string.cc
@@ -14,3 +14,20 @@ class Solution {
 };
 // 2781 ms
 // 610.67 MB
+
+class Solution {
+ public:
+  int countDistinct(string s) {
+    unordered_set<string> st;
+    int n = s.size();
+    for (int i = 0; i < n; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        st.insert(s.substr(i, j));
+      }
+    }
+
+    return st.size();
+  }
+};
+// 2861 ms
+// 612.39 MB
