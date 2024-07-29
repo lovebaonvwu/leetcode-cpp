5c322ae8076823f27e20a1d44a75063d43752bb4
lovebaonvwu <lovebaonvwu@gmail.com>
2024-07-29 09:07:12 +0800

    1230. Toss Strange Coins

--- 2024/toss_strange_coins.cc
+++ 2024/toss_strange_coins.cc
@@ -20,3 +20,26 @@ class Solution {
 };
 // 31 ms
 // 57.53 MB
+
+class Solution {
+ public:
+  double probabilityOfHeads(vector<double>& prob, int target) {
+    int n = prob.size();
+    vector<vector<double>> dp(n + 1, vector<double>(target + 2));
+    dp[1][1] = prob[0];
+    dp[1][0] = 1 - prob[0];
+
+    for (int i = 2; i <= n; ++i) {
+      for (int j = 0; j <= min(target, i); ++j) {
+        dp[i][j] = dp[i - 1][j] * (1 - prob[i - 1]);
+        if (j > 0) {
+          dp[i][j] += dp[i - 1][j - 1] * prob[i - 1];
+        }
+      }
+    }
+
+    return dp[n][target];
+  }
+};
+// 15 ms
+// 57.84 MB
