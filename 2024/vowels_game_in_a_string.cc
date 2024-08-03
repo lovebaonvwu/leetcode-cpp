36983df0845b48a2b9c187bb9b44f4a077e0a39f
Hank <lovebaonvwu@gmail.com>
2024-08-03 22:45:34 +0800

    3227. Vowels Game in a String

--- 2024/vowels_game_in_a_string.cc
+++ 2024/vowels_game_in_a_string.cc
@@ -13,3 +13,18 @@ public:
 };
 // 44 ms
 // 17.32 MB
+
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        for (auto c : s) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+// 31 ms
+// 17.32 MB
