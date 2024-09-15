7d566f73514b10a0ea2058e4f37f075a1193bf2d
Hank <lovebaonvwu@gmail.com>
2024-09-15 21:55:42 +0800

    3285. Find Indices of Stable Mountains

---
+++ 2024/find_indices_of_stable_mountains.cc
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> stableMountains(vector<int>& height, int threshold) {
+        vector<int> ans;
+        for (int i = 1; i < height.size(); ++i) {
+            if (height[i - 1] > threshold) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
+// 4 ms
+// 27.59 MB
