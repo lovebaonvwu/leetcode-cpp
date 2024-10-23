bb4020d05f8e03a0faa1af79fbd4409fb062ee2b
Hank <lovebaonvwu@gmail.com>
2024-10-23 08:59:42 +0800

    2641. Cousins in Binary Tree II

--- 2023/cousins_in_binary_tree_ii.cc
+++ 2023/cousins_in_binary_tree_ii.cc
@@ -73,3 +73,86 @@ class Solution {
 };
 // Runtime 695 ms
 // Memory 294.9 MB
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) 
+    {
+        vector<int> levelSums;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) 
+        {
+            int cnt = q.size();
+            int sum = 0;
+            while (cnt-- > 0)
+            {
+                auto node = q.front();
+                q.pop();
+
+                sum += node->val;
+                if (node->left)
+                {
+                    q.push(node->left);
+                }
+
+                if (node->right)
+                {
+                    q.push(node->right);
+                }
+            }
+
+            levelSums.push_back(sum);
+        }
+
+        q.push(root);
+        root->val = 0;
+        int levelIndex = 1;
+        while (!q.empty()) {
+            int cnt = q.size();
+            while (cnt-- > 0) {
+                auto node = q.front();
+                q.pop();
+
+                int sum = 0;
+                if (node->left) {
+                    sum += node->left->val;
+                }
+
+                if (node->right) {
+                    sum += node->right->val;
+                }
+
+                if (node->left) {
+                    node->left->val = levelSums[levelIndex] - sum;
+                    q.push(node->left);
+                }
+
+                if (node->right) {
+                    node->right->val = levelSums[levelIndex] - sum;
+                    q.push(node->right);
+                }
+            }
+
+            ++levelIndex;
+        }
+
+        return root;
+    }
+private:
+};
+// 10 ms
+// 323.95 MB
