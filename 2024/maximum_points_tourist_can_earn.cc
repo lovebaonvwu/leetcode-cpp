5d17b1f8cb83a7376645a7d9d22b93a60207372a
Hank <lovebaonvwu@gmail.com>
2024-10-28 10:13:50 +0800

    3332. Maximum Points Tourist Can Earn

--- 2024/maximum_points_tourist_can_earn.cc
+++ 2024/maximum_points_tourist_can_earn.cc
@@ -31,3 +31,24 @@ public:
 };
 // 1462 ms
 // 58.70 MB
+
+class Solution {
+public:
+    int maxScore(int n, int k, vector<vector<int>>& stayScore, vector<vector<int>>& travelScore) {
+        vector<vector<int>> dp(k, vector<int>(n));
+        int ans = 0;
+        for (int day = 0; day < k; ++day) {
+            for (int city = 0; city < n; ++city) {
+                dp[day][city] = max(dp[day][city], stayScore[day][city] + (day > 0 ? dp[day - 1][city] : 0));
+                for (int dest = 0; dest < n; ++dest) {
+                    dp[day][dest] = max(dp[day][dest], travelScore[city][dest] + (day > 0 ? dp[day - 1][city] : 0));
+                }
+            }
+        }
+
+        return *max_element(begin(dp[k - 1]), end(dp[k - 1]));
+    }
+};
+// 363 ms
+// 58.43 MB
+
