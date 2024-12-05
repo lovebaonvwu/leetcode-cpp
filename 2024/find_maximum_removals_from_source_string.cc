e06c7b5a5fe27d5287f380785f56059d3a810945
Hank <lovebaonvwu@gmail.com>
2024-12-05 09:38:43 +0800

    3316. Find Maximum Removals From Source String

--- 2024/find_maximum_removals_from_source_string.cc
+++ 2024/find_maximum_removals_from_source_string.cc
@@ -48,3 +48,53 @@ public:
 // 525 ms
 // 95.08 MB
 
+class Solution {
+public:
+    int maxRemovals(string source, string pattern, vector<int>& targetIndices) {
+        unordered_set<int> st(begin(targetIndices), end(targetIndices));
+
+        int m = source.size();
+        int n = pattern.size();
+        
+        vector<vector<int>> cached(m, vector<int>(n, -1));
+
+        function<int(int, int)> dfs = [&](int s, int p) -> int {
+            if (s == m) {
+                return p < n ? INT_MIN : 0;
+            }
+
+            if (p == n) {
+                int ret = 0;
+                for (; s < m; ++s) {
+                    if (st.count(s)) {
+                        ++ret;
+                    }
+                }
+
+                return ret;
+            }
+
+            if (cached[s][p] != -1) {
+                return cached[s][p];
+            }
+
+            int ret = INT_MIN;
+
+            if (source[s] == pattern[p]) {
+                ret = max(ret, dfs(s + 1, p + 1));
+            } else {
+                ret = max(ret, dfs(s + 1, p));
+            }
+
+            if (st.count(s)) {
+                ret = max(ret, dfs(s + 1, p) + 1);
+            }
+
+            return cached[s][p] = ret;
+        };
+
+        return dfs(0, 0);
+    }
+};
+// 295 ms
+// 95.07 MB
