6f1c8963b0a79f76555153166d9a63b05da47408
Hank <lovebaonvwu@gmail.com>
2024-12-22 16:43:31 +0800

    3392. Count Subarrays of Length Three With a Condition'

---
+++ 2024/count_subarrays_of_length_three_with_a_condition.cc
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            if ((nums[i] + nums[i + 2]) * 2 == nums[i + 1]) {
+                ++ans;
+            }
+        }
+
+        return ans;
+    }
+};
+// 0 ms
+// 48.35 MB
