13e714f3dd0663dd8379110f1ab9a4b24970c28a
Hank <lovebaonvwu@gmail.com>
2025-04-01 11:21:24 +0800

    10. Regular Expression Matching

---
+++ 2025/regular_expression_matching.cc
@@ -0,0 +1,33 @@
+class Solution {
+    public:
+        bool isMatch(string s, string p) {
+            int m = s.size();
+            int n = p.size();
+    
+            vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
+            dp[0][0] = true;
+    
+            for (int j = 1; j <= n; ++j) {
+                if (p[j - 1] == '*') {
+                    dp[0][j] = dp[0][j - 2];
+                }
+            }
+    
+            for (int i = 1; i <= m; ++i) {
+                for (int j = 1; j <= n; ++j) {
+                    if (s[i - 1] == p[j - 1] || p[j - 1] == '.') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else if (p[j - 1] == '*') {
+                        dp[i][j] = dp[i][j - 2];
+                        if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
+                            dp[i][j] = dp[i][j] | dp[i - 1][j];
+                        }
+                    } 
+                }
+            }
+    
+            return dp[m][n];
+        }
+    };
+    // Time: O(m * n)
+    // Space: O(m * n)
