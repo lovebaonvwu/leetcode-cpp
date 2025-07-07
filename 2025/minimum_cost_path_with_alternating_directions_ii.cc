cd1698d9b19970a81e0f84a42912819b49ca472d
Hank <lovebaonvwu@gmail.com>
2025-07-07 09:00:11 +0800

    3603. Minimum Cost Path with Alternating Directions II

---
+++ 2025/minimum_cost_path_with_alternating_directions_ii.cc
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    long long minCost(int m, int n, vector<vector<int>> &waitCost)
+    {
+        vector<vector<long long>> dp(m, vector<long long>(n));
+
+        dp[0][0] = 1;
+
+        for (int j = 1; j < n; ++j)
+        {
+            dp[0][j] = (0 + 1) * (j + 1) + waitCost[0][j] + dp[0][j - 1];
+        }
+
+        for (int i = 1; i < m; ++i)
+        {
+            dp[i][0] = (i + 1) * (0 + 1) + waitCost[i][0] + dp[i - 1][0];
+        }
+
+        for (int i = 1; i < m; ++i)
+        {
+            for (int j = 1; j < n; ++j)
+            {
+                dp[i][j] = (i + 1) * (j + 1) + waitCost[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[m - 1][n - 1] - waitCost[m - 1][n - 1];
+    }
+};
+// Time: O(m * n)
+// Space: O(m * n)
